8dcf05741225d17991eec02903f7325eccfa0cb4
Michael Fiano <michael.fiano@gmail.com>
2017-10-12 03:16:31 -0400

    print-object method for game-objects

--- src/game-object.lisp
+++ src/game-object.lisp
@@ -6,3 +6,7 @@
    (%components :accessor components
                 :initarg :components
                 :initform nil)))
+
+(defmethod print-object ((object game-object) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "~a" (id object))))
