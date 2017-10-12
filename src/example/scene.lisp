606016284e11017129c31dbd942cf6de391b87f7
Michael Fiano <michael.fiano@gmail.com>
2017-10-11 22:54:19 -0400

    Renaming, typos, and minor miscellaneous fixes to the possible scene dsl expansion code.

--- src/example/scene.lisp
+++ src/example/scene.lisp
@@ -1,27 +1,7 @@
-;; I think this entire file should just be Lisp with the exception of
-;; the LAST FORM, which is the scene tree form, this allows you to
-;; write any additional code you need in here that you might need.
-;;
-;; I think this file should be named scene.scn or something too.
-
 (defun random-shots (base extra)
   (+ base (random extra)))
 
-;; The scene tree is the LAST form in this DSL
-
-;; I suggest using <name> for the gobjs names and enforcing that. I have
-;; two string reasons:
-;;
-;; A) <value> is ONLY an entire reference to a game object as opposed
-;; to :value, which could stand for itself OR a gobj name OR a initarg.
-;;
-;; B) symbol-macrolet will do the wrong thing in the case where the gobj name
-;; coincides with a slot name in this form (and maybe in other places, like
-;; using keywords for tag names, etc, etc, etc).
-
-(<universe> ;; This must always be present and first.
- ((transform)) ;; along with this.
- (<player-ship>
+((<player-ship>
   ((transform))
   (<turret>
    ((transform :translation/current (vec 1 2 0))
@@ -37,144 +17,128 @@
      (gun :shots (random-shots 5 5)
           :type :homing))))))
 
-l;; Normally EOF here.
+;; Normally EOF here.
 
-;; ----------------------------------------------------
-;; Here is a possible expansion of the above scene tree into real CL.
-;; Once we implment the component and gobj stuff, we should just see if this
-;; executes in its raw form, if it works like how we like it, then we write the
-;; macro to generate this.
+;;; Possible expansion
+
+;; Here is a possible expansion of the above scene tree into real CL. Once we
+;; implment the component and gobj stuff, we should just see if this executes in
+;; its raw form, if it works like how we like it, then we write the macro to
+;; generate this.
 ;;
 ;; In short, I propose we put this progn into a DEFUN called "make-scene-tree"
 ;; and see if it works when we call it. :)
 
 (progn
-  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; all of the stuff before the last form, verbatim.
-  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; butlast forms
+
   (defun random-shots (base extra)
     (+ base (random extra)))
 
-  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; The last scene-tree form, reimagined....
-  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; last form (scene dsl) [possible] expansion
 
-  ;; NOTE: Whenever you see initarg-* it means a gensymed form.
-  ;; NOTE: Other ones should be fixed up to be gensymed, but I haven't done
-  ;; that yet.
+  ;; NOTE: Whenever you see initarg-* it means a gensymed form. NOTE: Other ones
+  ;; should be fixed up to be gensymed, but I haven't done that yet.
 
-  (let* ((num-objs 5)
-         (objs (make-hash-table))
-         (obj-names `(<universe> <player-ship> <turret> <laser> <missle>)))
-    ;; First, create the empty game objects so I can have real
-    ;; references to them.
-    (loop :for name :in obj-names :do
-      (setf (gethash name objs) (make-instance 'game-object)))
+  (let ((object-count 5) ; TODO: currently unused...needed?
+        (objects (make-hash-table))
+        (object-names `(<universe> <player-ship> <turret> <laser> <missle>)))
+    ;; First, create the empty game objects so I can have real references to
+    ;; them.
+    (loop :for name :in object-names
+          :do (setf (gethash name objects) (make-instance 'game-object)))
 
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;;;; Initializating <universe>
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;; add component transform
-    (let ((initarg-gobj (gethash '<universe> objs)))
-      (add-component (gethash '<universe> objs)
+;;; Initialize <universe>
+
+    (let ((initarg-game-object (gethash '<universe> objects)))
+      (add-component (gethash '<universe> objects)
                      (make-instance 'transform
-                                    :game-object initarg-gobj)))
+                                    :game-object initarg-game-object)))
 
-    ;; Each gobj os processed one by one in tree order.
-    ;; Each gob then has a pile of LET forms around each creation and
-    ;; insertion od the component. The LET form binds the values of the
-    ;; initarg plist and then assignes them to the initarg.
+    ;; Each game-object is processed one by one in tree order. Each then has a
+    ;; pile of LET forms around each creation and insertion of the component.
+    ;; The LET form binds the values of the initarg plist and then assigns them
+    ;; to the initarg.
 
+;;; Initialize <player-ship>
 
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;;;; Initializating <player-ship>
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;; add component transform.
-    (let ((initarg-gobj (gethash '<player-ship> objs)))
-      (add-component (gethash '<player-ship> objs)
+    (let ((initarg-game-object (gethash '<player-ship> objects)))
+      (add-component (gethash '<player-ship> objects)
                      (make-instance 'transform
-                                    :game-object initarg-gobj)))
+                                    :game-object initarg-game-object)))
 
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;;;; Initializaing <turret>
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;; add component transform
-    (let ((initarg-gobj (gethash '<turret> objs))
+;;; Initialize <turret>
+
+    (let ((initarg-game-object (gethash '<turret> objects))
           (initarg-translation/current (vec 1 2 0)))
       (add-component
-       (gethash '<turret> objs)
+       (gethash '<turret> objects)
        (make-instance 'transform
-                      :game-object initarg-gobj
+                      :game-object initarg-game-object
                       :translation/current initarg-translation/current)))
 
-    ;; add component gun-manager
-    (let ((initarg-gobj (gethash '<turrent> objs))
+    (let ((initarg-game-object (gethash '<turrent> objects))
           (initarg-active-gun 0)
-          (initarg-guns (vector (gethash '<laser> objs)
-                                (gethash '<missle> objs))))
-      (add-component (gethash '<turret> objs)
+          (initarg-guns (vector (gethash '<laser> objects)
+                                (gethash '<missle> objects))))
+      (add-component (gethash '<turret> objects)
                      (make-instance 'gun-manager
-                                    :game-object initarg-gobj
+                                    :game-object initarg-game-object
                                     :active-gun initarg-active-gun
                                     :guns initarg-guns)))
 
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;;;; Initializing <laser>
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;; add component transform
-    (let ((initarg-gobj (gethash '<laser> objs)))
-      (add-component (gethash '<laser> objs)
+;;; Initialize <laser>
+
+    (let ((initarg-game-object (gethash '<laser> objects)))
+      (add-component (gethash '<laser> objects)
                      (make-instance 'transform
-                                    :game-object initarg-obj)))
-    ;; add component gun
-    (let ((initarg-gobj (gethash '<laser> objs))
+                                    :game-object initarg-game-object)))
+
+    (let ((initarg-game-object (gethash '<laser> objects))
           (initarg-shots 10)
           (initarg-type :beam))
-      (add-component (gethash '<laser> objs)
+      (add-component (gethash '<laser> objects)
                      (make-instance 'gun
-                                    :game-object initarg-gobj
+                                    :game-object initarg-game-object
                                     :shots initarg-shots
                                     :type initarg-type)))
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;;;; Initializing <missle>
-    ;;;; ;;;;;;;;;;;;;;;;;;;
-    ;; add component transform
-    (let ((initarg-gobj (gethash '<missle> objs)))
-      (add-component (gethash '<missle> objs)
+
+;;; Initialize <missle>
+
+    (let ((initarg-game-object (gethash '<missle> objects)))
+      (add-component (gethash '<missle> objects)
                      (make-instance 'transform
-                                    :game-object initarg-obj)))
-    ;; add component gun
-    (let ((initarg-gobj (gethash '<missle> objs))
+                                    :game-object initarg-game-object)))
+
+    (let ((initarg-game-object (gethash '<missle> objects))
           (initarg-shots (random-shots 5 5))
           (initarg-type :homing))
-      (add-component (gethash '<missle> objs)
+      (add-component (gethash '<missle> objects)
                      (make-instance 'gun
-                                    :game-object initarg-gobj
-                                    :shots initarg-gobj
+                                    :game-object initarg-game-object
+                                    :shots initarg-shots
                                     :type initarg-type)))
 
 
-    ;;;; Now, we wire together the actual scene tree through the
-    ;;;; transform components. The transform component has the
-    ;;;; interface to and keeps the scene tree.
+;;; Now, we wire together the actual scene tree through the transform
+;;; components. The transform component has the interface to and keeps the scene
+;;; tree.
 
     (add-child
-     ;; parent
-     (get-component 'transform (gethash '<universe> objs))
-     ;; child
-     (get-component 'transform (gethash '<player-ship> objs)))
+     (get-component 'transform (gethash '<universe> objects))
+     (get-component 'transform (gethash '<player-ship> objects)))
 
     (add-child
-     (get-component 'transform (gethash '<player-ship> objs))
-     (get-component 'transform (gethash '<turret> objs)))
+     (get-component 'transform (gethash '<player-ship> objects))
+     (get-component 'transform (gethash '<turret> objects)))
 
     (add-child
-     (get-component 'transform (gethash '<turret> objs))
-     (get-component 'transform (gethash '<laser> objs)))
+     (get-component 'transform (gethash '<turret> objects))
+     (get-component 'transform (gethash '<laser> objects)))
 
     (add-child
-     (get-component 'transform (gethash '<turret> objs))
-     (get-component 'transform (gethash '<missle> objs)))
+     (get-component 'transform (gethash '<turret> objects))
+     (get-component 'transform (gethash '<missle> objects)))
 
-    ;; and now return the scene tree as a values with the root noted:
-    (values objs '<universe>)))
+    (values objects '<universe>)))
