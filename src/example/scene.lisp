8c27e1bb09618c7be29e28e122ec9e48570e4acb
Peter Keller <psilord@cs.wisc.edu>
2017-10-12 03:32:12 -0500

    Added the symbol-macrolet to use the actual symbol names the player provided to remove the need to edit any player evaluable forms in the DSL. This version is extremely close or identical to what a macro would generate. But, after points 3 and 4 in the org doc, there might be more changes to the overall structure of this example expansion.

--- src/example/scene.lisp
+++ src/example/scene.lisp
@@ -51,115 +51,126 @@
       (dolist (name tmp-object-names)
         (setf (gethash name tmp-objects) (make-instance 'game-object :id name)))
 
+      ;; This symbol-macrolet allows unedited use of the user's
+      ;; evaluable forms from the DSL.  We actually use the names for
+      ;; the gobjs as supplied by the user as "symbols" in the future
+      ;; forms. I suppose we might be able to use a LET here, but my
+      ;; brain insists upon this for now, and I'm tired. :)
+      (symbol-macrolet ((<universe> (gethash '<universe> tmp-objects))
+                        (<player-ship> (gethash '<player-ship> tmp-objects))
+                        (<turret> (gethash '<turret> tmp-objects))
+                        (<laser> (gethash '<laser> tmp-objects))
+                        (<missle> (gethash '<missle> tmp-objects)))
+
 ;;; Create all default components for all game-object to all future reference
 ;;; to them in the user values in the DSL are valid.
 
 ;;; Create default components for <universe>
-      (insert-default-components (gethash '<universe> tmp-objects)
-                                 (list (make-transform)))
+        (insert-default-components <universe>
+                                   (list (make-transform)))
 ;;; Create default components for <player-ship>
-      (insert-default-components (gethash '<player-ship> tmp-objects)
-                                 (list (make-transform)))
+        (insert-default-components <player-ship>
+                                   (list (make-transform)))
 ;;; Create default components for <turret>
-      (insert-default-components (gethash '<turret> tmp-objects)
-                                 (list (make-transform)
-                                       (make-gun-manager)))
+        (insert-default-components <turret>
+                                   (list (make-transform)
+                                         (make-gun-manager)))
 ;;; Create default components for <laser>
-      (insert-default-components (gethash '<laser> tmp-objects)
-                                 (list (make-transform)
-                                       (make-gun)))
+        (insert-default-components <laser>
+                                   (list (make-transform)
+                                         (make-gun)))
 ;;; Create default components for <missle>
-      (insert-default-components (gethash '<missle> tmp-objects)
-                                 (list (make-transform)
-                                       (make-gun)))
+        (insert-default-components <missle>
+                                   (list (make-transform)
+                                         (make-gun)))
 
 ;;; Then reinitialize all components with user values.
 
 ;;; Initialize <universe>
-      (let ((initarg-game-object (gethash '<universe> tmp-objects)))
-        (reinitialize-instance
-         (get-component 'transform (gethash '<universe> tmp-objects))
-         :game-object initarg-game-object))
+        (let ((initarg-game-object <universe>))
+          (reinitialize-instance
+           (get-component 'transform <universe>)
+           :game-object initarg-game-object))
 
 ;;; Initialize <player-ship>
 
-      (let ((initarg-game-object (gethash '<player-ship> tmp-objects)))
-        (reinitialize-instance
-         (get-component 'transform (gethash '<player-ship> tmp-objects))
-         :game-object initarg-game-object))
+        (let ((initarg-game-object <player-ship>))
+          (reinitialize-instance
+           (get-component 'transform <player-ship>)
+           :game-object initarg-game-object))
 
 ;;; Initialize <turret>
 
-      (let ((initarg-game-object (gethash '<turret> tmp-objects))
-            (initarg-translation/current (vec 1 2 0)))
-        (reinitialize-instance
-         (get-component 'transform (gethash '<turret> tmp-objects))
-         :game-object initarg-game-object
-         :translation/current initarg-translation/current))
+        (let ((initarg-game-object <turret>)
+              (initarg-translation/current (vec 1 2 0)))
+          (reinitialize-instance
+           (get-component 'transform <turret>)
+           :game-object initarg-game-object
+           :translation/current initarg-translation/current))
 
-      (let ((initarg-game-object (gethash '<turrent> tmp-objects))
-            (initarg-active-gun 0)
-            (initarg-guns
-              (vector (get-component 'gun (gethash '<laser> tmp-objects))
-                      (get-component 'gun (gethash '<missle> tmp-objects)))))
+        (let ((initarg-game-object <turret>)
+              (initarg-active-gun 0)
+              (initarg-guns
+                (vector (get-component 'gun <laser>)
+                        (get-component 'gun <missle>))))
 
-        (reinitialize-instance
-         (get-component 'gun-manager (gethash '<turret> tmp-objects))
-         :game-object initarg-game-object
-         :active-gun initarg-active-gun
-         :guns initarg-guns))
+          (reinitialize-instance
+           (get-component 'gun-manager <turret>)
+           :game-object initarg-game-object
+           :active-gun initarg-active-gun
+           :guns initarg-guns))
 
 ;;; Initialize <laser>
 
-      (let ((initarg-game-object (gethash '<laser> tmp-objects)))
-        (reinitialize-instance
-         (get-component 'transform (gethash '<laser> tmp-objects))
-         :game-object initarg-game-object))
+        (let ((initarg-game-object <laser>))
+          (reinitialize-instance
+           (get-component 'transform <laser>)
+           :game-object initarg-game-object))
 
-      (let ((initarg-game-object (gethash '<laser> tmp-objects))
-            (initarg-shots 10)
-            (initarg-shot-type :beam))
+        (let ((initarg-game-object <laser>)
+              (initarg-shots 10)
+              (initarg-shot-type :beam))
 
-        (reinitialize-instance
-         (get-component 'gun (gethash '<laser> tmp-objects))
-         :game-object initarg-game-object
-         :shots initarg-shots
-         :shot-type initarg-shot-type))
+          (reinitialize-instance
+           (get-component 'gun <laser>)
+           :game-object initarg-game-object
+           :shots initarg-shots
+           :shot-type initarg-shot-type))
 
 
 ;;; Initialize <missle>
 
-      (let ((initarg-game-object (gethash '<missle> tmp-objects)))
-        (reinitialize-instance
-         (get-component 'transform (gethash '<missle> tmp-objects))
-         :game-object initarg-game-object))
+        (let ((initarg-game-object <missle>))
+          (reinitialize-instance
+           (get-component 'transform <missle>)
+           :game-object initarg-game-object))
 
-      (let ((initarg-game-object (gethash '<missle> tmp-objects))
-            (initarg-shots (random-shots 5 5))
-            (initarg-shot-type :homing))
+        (let ((initarg-game-object <missle>)
+              (initarg-shots (random-shots 5 5))
+              (initarg-shot-type :homing))
 
-        (reinitialize-instance
-         (get-component 'gun (gethash '<missle> tmp-objects))
-         :game-object initarg-game-object
-         :shots initarg-shots
-         :shot-type initarg-shot-type))
+          (reinitialize-instance
+           (get-component 'gun <missle>)
+           :game-object initarg-game-object
+           :shots initarg-shots
+           :shot-type initarg-shot-type))
 
 ;;; Create the scene tree
 
-      (add-child
-       (get-component 'transform (gethash '<universe> tmp-objects))
-       (get-component 'transform (gethash '<player-ship> tmp-objects)))
+        (add-child
+         (get-component 'transform <universe>)
+         (get-component 'transform <player-ship>))
 
-      (add-child
-       (get-component 'transform (gethash '<player-ship> tmp-objects))
-       (get-component 'transform (gethash '<turret> tmp-objects)))
+        (add-child
+         (get-component 'transform <player-ship>)
+         (get-component 'transform <turret>))
 
-      (add-child
-       (get-component 'transform (gethash '<turret> tmp-objects))
-       (get-component 'transform (gethash '<laser> tmp-objects)))
+        (add-child
+         (get-component 'transform <turret>)
+         (get-component 'transform <laser>))
 
-      (add-child
-       (get-component 'transform (gethash '<turret> tmp-objects))
-       (get-component 'transform (gethash '<missle> tmp-objects)))
+        (add-child
+         (get-component 'transform <turret>)
+         (get-component 'transform <missle>))
 
-      (values tmp-objects '<universe>))))
+        (values tmp-objects '<universe>)))))
