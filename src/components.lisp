b01d7ef85d19a4cfaad75dd1c5179ee94310ceb2
Peter Keller <psilord@cs.wisc.edu>
2017-10-16 01:36:11 -0500

    Renamed comp-type variable to component-type.

--- src/components.lisp
+++ src/components.lisp
@@ -6,9 +6,9 @@
    (%game-object :accessor game-object
                  :initarg :game-object)))
 
-(defgeneric make-component (comp-type &rest args)
-  (:method ((comp-type symbol) &rest args)
-    (apply #'make-instance comp-type args)))
+(defgeneric make-component (component-type &rest args)
+  (:method ((component-type symbol) &rest args)
+    (apply #'make-instance component-type args)))
 
 (defgeneric add-component (game-object component)
   (:method ((game-object game-object) (component component))
@@ -16,20 +16,20 @@
     (push component (gethash (class-name (class-of component))
                              (components-by-type game-object)))))
 
-(defgeneric get-component (comp-type game-object)
-  (:documentation "Return the _first found_ component of COMP-TYPE in
+(defgeneric get-component (component-type game-object)
+  (:documentation "Return the _first found_ component of COMPONENT-TYPE in
 the GAME-OBJECT.  If there are multiple components of this type, it is
 unknown which one will be returned first. Usually, there is only one
-component of any given COMP-TYPE in a GAME-OBJECT.")
-  (:method ((comp-type symbol) (game-object game-object))
-    (car (gethash comp-type
+component of any given COMPONENT-TYPE in a GAME-OBJECT.")
+  (:method ((component-type symbol) (game-object game-object))
+    (car (gethash component-type
                   (components-by-type game-object)))))
 
-(defgeneric get-components (comp-type game-object)
-  (:documentation "Return a list of the components with this COMP-TYPE in
+(defgeneric get-components (component-type game-object)
+  (:documentation "Return a list of the components with this COMPONENT-TYPE in
 the GAME-OBJECT. Do not modify the structure of the returned list.")
-  (:method ((comp-type symbol) (game-object game-object))
-    (gethash comp-type (components-by-type game-object))))
+  (:method ((component-type symbol) (game-object game-object))
+    (gethash component-type (components-by-type game-object))))
 
 (defun add-multiple-components (game-object components)
   (dolist (component components)
