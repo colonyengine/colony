f95618dda4a098b65423974b3edfb64671811cd7
Peter Keller <psilord@cs.wisc.edu>
2017-10-16 01:30:02 -0500

    comments. Added lambda funciton around expansion in example/scene.lisp

--- src/example/scene.lisp
+++ src/example/scene.lisp
@@ -27,101 +27,107 @@
   (defun random-shots (base extra)
     (+ base (random extra)))
 
-  (let ((objects (make-hash-table)))
-    (dolist (name '(<universe> <player-ship> <turret> <laser> <missile>))
-      (setf (gethash name objects) (make-instance 'game-object
-						  :id name
-						  :state :initialize)))
+  ;; Called when we're ready to create all of the game-objects,
+  ;; components, and scene-tree and shove it all into core-state.
+  (lambda (core-state)
+    ;; Until this gets used in here...
+    (declare (ignorable core-state))
 
-    (let ((<universe> (gethash '<universe> objects))
-          (<player-ship> (gethash '<player-ship> objects))
-          (<turret> (gethash '<turret> objects))
-          (<laser> (gethash '<laser> objects))
-          (<missile> (gethash '<missile> objects)))
+    (let ((objects (make-hash-table)))
+      (dolist (name '(<universe> <player-ship> <turret> <laser> <missile>))
+        (setf (gethash name objects) (make-instance 'game-object
+                                                    :id name
+                                                    :state :initialize)))
 
-      (add-multiple-components
-       <universe>
-       (list (make-component 'transform :state :initialize)))
+      (let ((<universe> (gethash '<universe> objects))
+            (<player-ship> (gethash '<player-ship> objects))
+            (<turret> (gethash '<turret> objects))
+            (<laser> (gethash '<laser> objects))
+            (<missile> (gethash '<missile> objects)))
 
-      (add-multiple-components
-       <player-ship>
-       (list (make-component 'transform :state :initialize)
-             (make-component 'hit-points :state :initialize)))
+        (add-multiple-components
+         <universe>
+         (list (make-component 'transform :state :initialize)))
 
-      (add-multiple-components
-       <turret>
-       (list (make-component 'transform :state :initialize)
-             (make-component 'gun-manager :state :initialize)))
+        (add-multiple-components
+         <player-ship>
+         (list (make-component 'transform :state :initialize)
+               (make-component 'hit-points :state :initialize)))
 
-      (add-multiple-components
-       <laser>
-       (list (make-component 'transform :state :initialize)
-             (make-component 'gun :state :initialize)))
+        (add-multiple-components
+         <turret>
+         (list (make-component 'transform :state :initialize)
+               (make-component 'gun-manager :state :initialize)))
 
-      (add-multiple-components
-       <missile>
-       (list (make-component 'transform :state :initialize)
-             (make-component 'gun :state :initialize)))
+        (add-multiple-components
+         <laser>
+         (list (make-component 'transform :state :initialize)
+               (make-component 'gun :state :initialize)))
 
-      (reinitialize-instance
-       (get-component 'transform <universe>)
-       :game-object <universe>)
+        (add-multiple-components
+         <missile>
+         (list (make-component 'transform :state :initialize)
+               (make-component 'gun :state :initialize)))
 
-      (reinitialize-instance
-       (get-component 'transform <player-ship>)
-       :game-object <player-ship>)
+        (reinitialize-instance
+         (get-component 'transform <universe>)
+         :game-object <universe>)
 
-      (reinitialize-instance
-       (get-component 'hit-points <player-ship>)
-       :game-object <player-ship>
-       :hp 100)
+        (reinitialize-instance
+         (get-component 'transform <player-ship>)
+         :game-object <player-ship>)
 
-      (reinitialize-instance
-       (get-component 'transform <turret>)
-       :game-object <turret>
-       :translation/current (vec 1 2 0))
+        (reinitialize-instance
+         (get-component 'hit-points <player-ship>)
+         :game-object <player-ship>
+         :hp 100)
 
-      (reinitialize-instance
-       (get-component 'gun-manager <turret>)
-       :game-object <turret>
-       :active-gun 0
-       :guns (vector (get-component 'gun <laser>)
-                     (get-component 'gun <missile>)))
+        (reinitialize-instance
+         (get-component 'transform <turret>)
+         :game-object <turret>
+         :translation/current (vec 1 2 0))
 
-      (reinitialize-instance
-       (get-component 'transform <laser>)
-       :game-object <laser>)
+        (reinitialize-instance
+         (get-component 'gun-manager <turret>)
+         :game-object <turret>
+         :active-gun 0
+         :guns (vector (get-component 'gun <laser>)
+                       (get-component 'gun <missile>)))
 
-      (reinitialize-instance
-       (get-component 'gun <laser>)
-       :game-object <laser>
-       :shot-count 10
-       :shot-type :beam)
+        (reinitialize-instance
+         (get-component 'transform <laser>)
+         :game-object <laser>)
 
-      (reinitialize-instance
-       (get-component 'transform <missile>)
-       :game-object <missile>)
+        (reinitialize-instance
+         (get-component 'gun <laser>)
+         :game-object <laser>
+         :shot-count 10
+         :shot-type :beam)
 
-      (reinitialize-instance
-       (get-component 'gun <missile>)
-       :game-object <missile>
-       :shot-count (random-shots 5 5)
-       :shot-type :homing)
+        (reinitialize-instance
+         (get-component 'transform <missile>)
+         :game-object <missile>)
 
-      (add-child
-       (get-component 'transform <universe>)
-       (get-component 'transform <player-ship>))
+        (reinitialize-instance
+         (get-component 'gun <missile>)
+         :game-object <missile>
+         :shot-count (random-shots 5 5)
+         :shot-type :homing)
 
-      (add-child
-       (get-component 'transform <player-ship>)
-       (get-component 'transform <turret>))
+        (add-child
+         (get-component 'transform <universe>)
+         (get-component 'transform <player-ship>))
 
-      (add-child
-       (get-component 'transform <turret>)
-       (get-component 'transform <laser>))
+        (add-child
+         (get-component 'transform <player-ship>)
+         (get-component 'transform <turret>))
 
-      (add-child
-       (get-component 'transform <turret>)
-       (get-component 'transform <missile>))
+        (add-child
+         (get-component 'transform <turret>)
+         (get-component 'transform <laser>))
 
-      (values objects '<universe>))))
+        (add-child
+         (get-component 'transform <turret>)
+         (get-component 'transform <missile>))
+
+        (values objects '<universe>)))))
--- src/game-object.lisp
+++ src/game-object.lisp
@@ -12,10 +12,11 @@
    (%components :accessor components
                 :initarg :components
                 :initform (make-hash-table))
-   ;; A view into the components instances.
-   ;; Keyed by (reference to) instance, value is list of (reference
-   ;; to) instances (that are located in the components slot of this
-   ;; object).
+   ;; A view into the components instances.  Keyed by (reference to)
+   ;; instance, value is list of (reference to) instances (that are
+   ;; located in the components slot of this object). It is ok if the
+   ;; value is a list since it'll generally be the case noone searches
+   ;; in this list for anything on a common basis.
    (%components-by-type :accessor components-by-type
                         :initarg :components-by-type
                         :initform (make-hash-table))))
