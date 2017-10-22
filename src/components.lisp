b324fe6194b589a02c1b643f40e19e7330498b75
Michael Fiano <michael.fiano@gmail.com>
2017-10-22 02:40:28 -0400

    Define get-component in terms of get-components.

--- src/components.lisp
+++ src/components.lisp
@@ -16,19 +16,19 @@
     (push component (gethash (class-name (class-of component))
                              (components-by-type game-object)))))
 
+(defgeneric get-components (component-type game-object)
+  (:documentation "Return a list of the components with this COMPONENT-TYPE in
+the GAME-OBJECT. Do not modify the structure of the returned list.")
+  (:method ((component-type symbol) (game-object game-object))
+    (gethash component-type (components-by-type game-object))))
+
 (defgeneric get-component (component-type game-object)
   (:documentation "Return the _first found_ component of COMPONENT-TYPE in
 the GAME-OBJECT.  If there are multiple components of this type, it is
 unknown which one will be returned first. Usually, there is only one
 component of any given COMPONENT-TYPE in a GAME-OBJECT.")
   (:method ((component-type symbol) (game-object game-object))
-    (first (gethash component-type (components-by-type game-object)))))
-
-(defgeneric get-components (component-type game-object)
-  (:documentation "Return a list of the components with this COMPONENT-TYPE in
-the GAME-OBJECT. Do not modify the structure of the returned list.")
-  (:method ((component-type symbol) (game-object game-object))
-    (gethash component-type (components-by-type game-object))))
+    (first (get-components component-type game-object))))
 
 (defun add-multiple-components (game-object components)
   (dolist (component components)
