2b292d68c0d4ebcbfa994b498a6e3e7ee4bb270c
Peter Keller <psilord@cs.wisc.edu>
2017-10-22 01:54:41 -0500

    checkpointing commit

--- src/core-state.lisp
+++ src/core-state.lisp
@@ -1,12 +1,12 @@
 (in-package :gear)
 
 (defclass core-state ()
-  ((%game-object-initialize-db :accessor game-object-initialize-db
-                               :initarg :game-object-initialize-db
-                               :initform (make-hash-table))
-   (%game-object-active-db :accessor game-object-active-db
-                           :initarg :game-object-active-db
-                           :initform (make-hash-table))
+  ((%actor-initialize-db :accessor actor-initialize-db
+                         :initarg :actor-initialize-db
+                         :initform (make-hash-table))
+   (%actor-active-db :accessor actor-active-db
+                     :initarg :actor-active-db
+                     :initform (make-hash-table))
    (%component-initialize-view :accessor component-initialize-view
                                :initarg :component-initialize-view
                                :initform (make-hash-table))
@@ -14,5 +14,20 @@
                            :initarg :component-active-view
                            :initform (make-hash-table))
    (%scene-tree :accessor scene-tree
-		:initarg :scene-tree
-		:initform NIL)))
+                :initarg :scene-tree
+                :initform NIL)))
+
+(defun make-core-state (&rest initargs)
+  (make-instance 'core-state initargs))
+
+
+(defun add-initializing-actor (core-state actor
+                               initializer-thunk-list)
+
+  ;; Store initaizing actor
+  (setf (gethash actor (game-object-initialize-db core-state)) actor)
+
+  ;; Store all associated components for actor.
+  (maphash (lambda (k v)
+             (setf (gethash k (component-initialize-view core-state)) v))
+           (components actor)))
