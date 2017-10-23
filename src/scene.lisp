aa53f4c077a078f0ceabd4c13000e46dfdd7f18a
Peter Keller <psilord@cs.wisc.edu>
2017-10-23 17:38:59 -0500

    helper functions to write parse-scene

--- src/scene.lisp
+++ src/scene.lisp
@@ -14,12 +14,43 @@
     (list actor components children)))
 
 (defun collect-actor-symbols (scene-spec)
-  (append
-   '(@universe)
-   (remove-if
-    (lambda (x) (or (not (symbolp x))
-               (not (eq (char (symbol-name x) 0) #\@))))
-    (flatten scene-spec))))
+  (remove-duplicates (append
+                      '(@universe)
+                      (remove-if
+                       (lambda (x) (or (not (symbolp x))
+                                       (not (eq (char (symbol-name x) 0) #\@))))
+                       (flatten scene-spec)))))
+
+;; This does double duty, it builds a list of components for each actor
+;; which includes the initform for that component. So, using first & cdr
+;; you can get all the info you need for each component for each actor.
+(defun collect-actor-component-info (form &optional (ht (make-hash-table)))
+  ;; First, collect all types/initforms for each actor at this level.
+  (loop :for actor :in form
+        :do
+           (let ((component-types (reverse (second actor))))
+             (loop :for comp-type :in component-types
+                   :do (push comp-type (gethash (first actor) ht)))))
+  ;; Then, for each child of each actor, go get more.
+  (loop :for actor :in form
+        :do (collect-actor-component-info (cddr actor) ht))
+  ht)
+
+;; Produce the names for the variables holding component thunk lists
+;; for that actor.
+(defun gen-actor-symbol-thunk-vars (actor-names)
+  (mapcar (lambda (an)
+            (symbolicate an "-INITIALIZE-COMPONENTS-" (gensym "LIST")))
+          actor-names))
+
+;; Collect the data and produce the form.
+(defun parse-scene (dsl)
+  (let* ((actor-names (collect-actor-symbols form))
+         (actor-name-thunk-vars (gen-actor-symbol-thunk-vars actor-names))
+         (actor-component-info (collect-actor-component-info dsl))
+         )
+    nil))
+
 
 (defmacro read-scene-file (file)
   `(let ((spec (read-spec-forms ,file)))
