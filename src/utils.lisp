406fef937109d770e21eec8fe63595eecfc62245
Michael Fiano <michael.fiano@gmail.com>
2017-10-24 18:10:33 -0400

    Changed a docstring.

--- src/utils.lisp
+++ src/utils.lisp
@@ -2,12 +2,9 @@
 
 (defun get-path (system &optional path)
   "Get the absolute path of a resource relative to the system designated by
-SYSTEM. This is needed to accurated refer to resources, such as a scene
-definition file, when the path to the system may be different than what it was
-before dumping an image. Thus, this function will return the correct full path
-to a file or sub-directory regardless of whether it is called interactively, or
-from a deployed binary image which would otherwise return a path to the system
-as it was before image dumping occured."
+SYSTEM. This is needed to refer to file resources on disk, in a method that
+works for both interactive development and dumped core images where system paths
+would otherwise refer to those on the host machine."
   (if uiop/image:*image-dumped-p*
       (truename
        (uiop/pathname:merge-pathnames*
