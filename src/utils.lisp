92b7e5bc49abf6ba25b6cd9fde66b38afd2062e2
Michael Fiano <michael.fiano@gmail.com>
2017-10-24 23:44:26 -0400

    Added some utility functions for finding/loading extension files.

--- src/utils.lisp
+++ src/utils.lisp
@@ -11,3 +11,20 @@ would otherwise refer to those on the host machine."
         path
         (uiop:pathname-directory-pathname (uiop:argv0))))
       (asdf/system:system-relative-pathname system path)))
+
+(defun map-files (path effect &key (filter (constantly t)) (recursive t))
+  (labels ((maybe-affect (file)
+             (when (funcall filter file)
+               (funcall effect file)))
+           (process-files (dir)
+             (map nil #'maybe-affect (uiop/filesystem:directory-files dir))))
+    (uiop/filesystem:collect-sub*directories
+     (uiop/pathname:ensure-directory-pathname path)
+     t recursive #'process-files)))
+
+(defun load-extensions ()
+  (map-files
+   path
+   (lambda (x) (load x :verbose t))
+   :filter (lambda (x) (or (string= (pathname-type x) "lisp")
+                      (string= (pathname-type x) "scene")))))
