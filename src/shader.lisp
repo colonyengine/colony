99c5c2e86fa2a8b4161e6672bde41207f8c2ae6c
Michael Fiano <michael.fiano@gmail.com>
2017-11-07 16:46:48 -0500

    Shader definition parsing finalized.

--- src/shader.lisp
+++ src/shader.lisp
@@ -1,60 +1,10 @@
 (in-package :gear)
 
-(defmethod kit.gl.shader:parse-shader-source-complex
-    ((key (eql :generate)) params shader-type shader-list)
-  (destructuring-bind (version stage function) params
-    (3bgl-shaders:generate-stage stage function :version version)))
+(defmacro shader-stages (&body body)
+  body)
 
-(defun %type-check-shader-symbol (symbol)
-  (or (find-symbol (symbol-name symbol) :gear-shaders)
-      (error "Shader stage ~a not defined in the :GEAR-SHADERS package."
-             symbol)))
-
-(defun %generate-stage-symbol (stage)
-  (make-keyword (symbolicate stage '-shader)))
-
-(defun %generate-stages (version data)
-  (loop :for (stage func) :in data
-        :collect `(shader ,func
-                          ,(%generate-stage-symbol stage)
-                          (:generate ,version
-                                     ,stage
-                                     ,(%type-check-shader-symbol func)))))
-
-(defun %generate-uniform-symbol (parts)
-  (make-keyword
-   (with-output-to-string (s)
-     (loop :for (part . rest) :on parts
-           :do (etypecase part
-                 (number (format s "[~a]" part))
-                 (symbol (format s "~a" part)))
-           :when rest
-             :do (format s ".")))))
-
-(defun %generate-uniform-list (&rest stages)
-  (remove-duplicates
-   (loop :for (stage func) :on stages :by #'cddr
-         :for uniforms = (nth-value 1 (3bgl-shaders:generate-stage
-                                       stage
-                                       (%type-check-shader-symbol func)
-                                       :expand-uniforms t))
-         :append (loop :for (lisp nil nil . properties) :in uniforms
-                       :for components = (getf properties :components)
-                       :append (mapcar #'%generate-uniform-symbol components)))))
-
-(defun %generate-program-stages (data)
-  (loop :for (stage func) :on data :by #'cddr
-        :collect (list (%generate-stage-symbol stage)
-                       (%type-check-shader-symbol func))))
-
-(defun %generate-programs (data)
-  (loop :for (name stages) :in data
-        :collect `(program ,name
-                           ,(apply #'%generate-uniform-list stages)
-                           ,@(%generate-program-stages stages))))
-
-(defmethod extension-file-types ((owner (eql 'shader)))
-  (list "shd"))
+(defmacro shader-programs (&body body)
+  body)
 
 (defmacro %with-shader-forms ((forms) &body body)
   `(dolist (form ,forms)
@@ -69,6 +19,10 @@
         ,@body))
     ,data
     :key ,key))
+
+(defun %type-check-shader-function (symbol)
+  (or (find-symbol (symbol-name symbol) :gear-shaders)
+      (error "Function ~a not defined in the :GEAR-SHADERS package." symbol)))
 
 (defun %type-check-stages (forms)
   (let ((fn-symbols))
@@ -85,7 +39,7 @@
 (defun %type-check-programs (forms)
   (let ((program-names))
     (%with-shader-forms (forms)
-      (loop :for (name  nil) :in data
+      (loop :for (name . nil) :in data
             :do (push name program-names))
       (%shader-symbol-duplicate-check (program-names #'identity)
         (error "Program ~s must not be defined more than once in all ~s forms."
@@ -100,47 +54,115 @@
                 :collect key :into stages
                 :finally (%check program stages)))))))
 
-(defun %collect-shader-stages (forms)
-  (flet ((%collect ()
-           (loop :for (nil nil . stages) :in forms
-                 :append (loop :for (stage fn) :in stages
-                               :append (list stage fn)))))
+(defun %generate-uniforms (&rest stages)
+  (flet ((%generate-uniform-symbol (parts)
+           (make-keyword
+            (with-output-to-string (s)
+              (loop :for (part . rest) :on parts
+                    :do (etypecase part
+                          (number (format s "[~a]" part))
+                          (symbol (format s "~a" part)))
+                    :when rest
+                      :do (format s "."))))))
+    (remove-duplicates
+     (loop :for (stage func) :on stages :by #'cddr
+           :for uniforms = (nth-value 1 (3bgl-shaders:generate-stage
+                                         stage
+                                         (%type-check-shader-function func)
+                                         :expand-uniforms t))
+           :append (loop :for (lisp nil nil . properties) :in uniforms
+                         :for components = (getf properties :components)
+                         :append (mapcar #'%generate-uniform-symbol
+                                         components))))))
+
+(defun %make-dictionary-programs (data)
+  (flet ((%generate-stages (stages)
+           (loop :for (stage func) :on stages :by #'cddr
+                 :collect `(,(make-keyword (symbolicate stage '-shader))
+                            ,(%type-check-shader-function func)))))
+    (loop :for (name stages) :in data
+          :collect `(:name ,name
+                     :uniforms ,(apply #'%generate-uniforms stages)
+                     :shaders ,(%generate-stages stages)))))
+
+(defun %make-dictionary-stages (data)
+  (loop :for (type . rest) :in data
+        :append (loop :for item :in rest
+                      :for (func . options) = item
+                      :for func-symbol = (%type-check-shader-function func)
+                      :collect
+                      `(,func-symbol
+                        ,(make-keyword (symbolicate type '-shader))
+                        (:generate ,(getf options :version)
+                                   ,type
+                                   ,func-symbol)))))
+
+(defun %process-stage-forms (forms)
+  (let ((result))
+    (loop :for (nil options . (stages)) :in forms
+          :for default-version = (getf options :default-version 330)
+          :do (loop :for (type . funcs) :in stages
+                    :do (loop :for func :in funcs
+                              :for options = (cdr (ensure-list func))
+                              :unless (getf options :version)
+                                :do (setf func (append
+                                                (list func)
+                                                `(:version ,default-version)))
+                              :do (push (list type func) result))))
     (loop :with table = (make-hash-table)
-          :for (stage . fns) :in (%collect)
-          :do (appendf (gethash stage table) fns)
+          :for (type func) :in result
+          :do (push func (gethash type table))
           :finally (return (nreverse (hash-table-alist table))))))
 
-(defun %collect-shader-programs (forms)
+(defun %process-program-forms (forms)
   (let ((programs-list))
     (%with-shader-forms (forms)
       (appendf programs-list data))
     programs-list))
 
 (defun %collect-shader-forms (path)
-  (let ((all-forms (collect-extension-forms 'shader path))
-        (stage-forms)
-        (program-forms))
-    (mapcar
-     (lambda (x)
-       (destructuring-bind (type options . rest) x
-         (declare (ignore rest))
-         (when (getf options :enabled)
-           (case type
-             (shader-stages (appendf stage-forms (list x)))
-             (shader-programs (appendf program-forms (list x)))))))
-     all-forms)
-    (%type-check-stages stage-forms)
-    (%type-check-programs program-forms)
-    (values (%collect-shader-stages stage-forms)
-            (%collect-shader-programs program-forms))))
+  (loop :for form :in (collect-extension-forms 'shader path)
+        :for (form-type . nil) = form
+        :when (eq form-type 'shader-stages)
+          :collect form :into stages
+        :when (eq form-type 'shader-programs)
+          :collect form :into programs
+        :finally (%type-check-stages stages)
+                 (%type-check-programs programs)
+                 (return (values (%process-stage-forms stages)
+                                 (%process-program-forms programs)))))
+
+(defmethod kit.gl.shader:parse-shader-source-complex
+    ((key (eql :generate)) params shader-type shader-list)
+  (destructuring-bind (version stage function) params
+    (3bgl-shaders:generate-stage stage function :version version)))
+
+(defmethod extension-file-type ((extension-type (eql 'shader)))
+  "shd")
 
 (defun make-shader-dictionary (extension-path)
   (multiple-value-bind (stages programs) (%collect-shader-forms extension-path)
     (kit.gl.shader:define-dictionary :shaders
-        (loop :for (type name uniforms . stages) :in (%generate-programs programs)
-              :collect (make-instance 'kit.gl.shader::program-source
-                                      :name name
-                                      :uniform-style :camel-case
-                                      :uniforms uniforms
-                                      :shaders stages))
-      :shaders (mapcar #'rest (%generate-stages 330 stages)))))
+        (loop :for program :in (%make-dictionary-programs programs)
+              :collect (apply #'make-instance 'kit.gl.shader::program-source
+                              :uniform-style :camel-case
+                              program))
+      :shaders (%make-dictionary-stages stages))))
+
+(defun compile-shaders (core-state)
+  (setf (shaders core-state)
+        (kit.gl.shader:compile-shader-dictionary :shaders))
+  (pushnew (shaders-modified-hook-generator core-state)
+           3bgl-shaders:*modified-function-hook*)
+  (dolist (func (shaders-modified core-state))
+    (slog:emit :shader.function.compiled func)))
+
+(defun shaders-modified-hook-generator (core-state)
+  (lambda (x)
+    (setf (shaders-modified core-state)
+          (union x (shaders-modified core-state)))))
+
+(defun maybe-recompile-shaders (core-state)
+  (when (shaders-modified core-state)
+    (compile-shaders core-state)
+    (setf (shaders-modified core-state) nil)))
