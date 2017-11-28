b63d0460dc50f2abd46689b1246ebc5d7e663bc8
Michael Fiano <michael.fiano@gmail.com>
2017-11-28 03:04:02 -0500

    VAO specs are now fully generated.
    The only thing left is to parse the mesh dsl forms, and fill the VBOs, with an
    interface for uploading data to VBO's by name.

--- src/vertex-data.lisp
+++ src/vertex-data.lisp
@@ -1,19 +1,21 @@
 (in-package :first-light)
 
-(defmacro define-vertex-layout (&body body)
+(defmacro define-vertex-groups (&body body)
   body)
 
-(defmacro define-vertex-spec (&body body)
+(defmacro define-vertex-layout (&body body)
   body)
 
 (defmacro define-mesh (&body body)
   body)
 
 (defclass vertex-metadata ()
-  ((%layouts :reader layouts)
-   (%specs :reader specs)))
+  ((%groups :reader groups
+            :initarg :groups)
+   (%layouts :reader layouts
+             :initarg :layouts)))
 
-(defclass vertex-layout-group ()
+(defclass vertex-group ()
   ((%id :reader id
         :initarg :id)
    (%divisor :reader divisor
@@ -21,94 +23,98 @@
    (%attrs :reader attrs
            :initarg :attrs)))
 
-(defclass vertex-spec ()
+(defclass vertex-layout ()
   ((%id :reader id
         :initarg :id)
-   (%layout-names :reader layout-names
-                  :initarg :layout-names)
-   (%buffer-order :accessor buffer-order
+   (%group-names :reader group-names
+                 :initarg :group-names)
+   (%group-attrs :reader group-attrs
+                 :initform (make-hash-table))
+   (%buffer-order :reader buffer-order
                   :initform nil)
-   (%layout-groups :accessor layout-groups
-                   :initform (make-hash-table))
+   (%buffer-divisors :reader buffer-divisors
+                     :initform (make-hash-table))
    (%primitive :reader primitive
                :initarg :primitive)
-   (%vao-declaration :accessor vao-declaration)))
+   (%vao-spec :accessor vao-spec)))
 
-(defun %make-layout-group (id divisor attrs)
-  (make-instance 'vertex-layout-group :id id :divisor divisor :attrs attrs))
+(defun %make-vertex-group (id divisor attrs)
+  (make-instance 'vertex-group :id id :divisor divisor :attrs attrs))
 
-(defun %make-vertex-spec (id names primitive)
-  (make-instance 'vertex-spec :id id :layout-names names :primitive primitive))
+(defun %make-vertex-layout (id names primitive)
+  (make-instance 'vertex-layout :id id :group-names names :primitive primitive))
 
-(defun %collect-vertex-layouts (path)
+(defun %collect-vertex-groups (path)
+  (let ((group-table (make-hash-table)))
+    (dolist (form (collect-extension-forms 'vertex path))
+      (when (eq (car form) 'define-vertex-groups)
+        (destructuring-bind (form-type name group-forms) form
+          (declare (ignore form-type))
+          (dolist (group-form group-forms)
+            (destructuring-bind (&key (id :mesh-data) (divisor 0) attrs)
+                group-form
+              (let ((group (%make-vertex-group id divisor attrs)))
+                (push group (gethash name group-table)))))
+          (reversef (gethash name group-table)))))
+    group-table))
+
+(defun %vertex-layout-add-buffer-id (vertex-layout buffer-id)
+  (with-slots (%buffer-order) vertex-layout
+    (unless (member buffer-id %buffer-order)
+      (push buffer-id %buffer-order)
+      (reversef %buffer-order))))
+
+(defun %vertex-layout-splice-group (vertex-layout group)
+  (symbol-macrolet ((attrs (gethash (id group) (group-attrs vertex-layout))))
+    (dolist (attr (attrs group))
+      (if-let ((existsp (find-if
+                         (lambda (x) (eq (first x) (first attr)))
+                         attrs)))
+        (setf (cdr existsp) (copy-seq (cdr attr)))
+        (appendf attrs (list attr))))))
+
+(defun %vertex-layout-add-buffer-divisor (vertex-layout group)
+  (setf (gethash (id group) (buffer-divisors vertex-layout)) (divisor group)))
+
+(defun %vertex-layout-generate (vertex-layout group-table)
+  (with-slots (%vao-spec) vertex-layout
+    (dolist (group-name (group-names vertex-layout))
+      (dolist (group (gethash group-name group-table))
+        (%vertex-layout-add-buffer-id vertex-layout (id group))
+        (%vertex-layout-add-buffer-divisor vertex-layout group)
+        (%vertex-layout-splice-group vertex-layout group)))
+    (setf %vao-spec (%create-vao-spec vertex-layout))))
+
+(defun %collect-vertex-layouts (path group-table)
   (let ((layouts (make-hash-table)))
     (dolist (form (collect-extension-forms 'vertex path))
       (when (eq (car form) 'define-vertex-layout)
-        (destructuring-bind (form-type name groups) form
-          (declare (ignore form-type))
-          (dolist (spec groups)
-            (destructuring-bind (&key (id :mesh-data) (divisor 0) attrs) spec
-              (let ((layout-group (%make-layout-group id divisor attrs)))
-                (appendf (gethash name layouts) (list layout-group))))))))
-    layouts))
-
-(defun %collect-vertex-specs (path layout-table)
-  (let ((specs (make-hash-table)))
-    (dolist (form (collect-extension-forms 'vertex path))
-      (when (eq (car form) 'define-vertex-spec)
         (destructuring-bind (form-type id body) form
           (declare (ignore form-type))
-          (destructuring-bind (&key layouts (primitive :triangles)) body
-            (let ((vertex-spec (%make-vertex-spec id layouts primitive)))
-              ;; populate the spec groups with additive layout groups
-              ;; TODO cleanup this mess
+          (destructuring-bind (&key groups (primitive :triangles)) body
+            (let ((vertex-layout (%make-vertex-layout id groups primitive)))
+              (%vertex-layout-generate vertex-layout group-table)
+              (setf (gethash id layouts) vertex-layout))))))
+    layouts))
 
-              ;; NOTE it is undefined behavior if a divisor is specified
-              ;; multiple times across the same group having different values.
-              ;; maybe we want to override it if this occurs? (last layout to
-              ;; set it in the additive merge wins).
-              (dolist (layout layouts)
-                (dolist (group-spec (gethash layout layout-table))
-                  (unless (member (id group-spec) (buffer-order vertex-spec))
-                    (appendf (buffer-order vertex-spec) (list (id group-spec))))
-                  (symbol-macrolet ((groups
-                                      (gethash (id group-spec)
-                                               (layout-groups vertex-spec))))
-                    (dolist (attr (attrs group-spec))
-                      (if-let ((existsp (find-if
-                                         (lambda (x) (eq (first x) (first attr)))
-                                         groups)))
-                        (setf (cdr existsp) (copy-seq (cdr attr)))
-                        (appendf groups (list attr)))))))
-              (setf (vao-declaration vertex-spec) (%create-vao-declaration vertex-spec))
-              ;; add the spec to the table
-              (setf (gethash id specs) vertex-spec))))))
-    specs))
-
-(defun %create-vao-declaration (spec)
+(defun %create-vao-spec (layout)
   (let ((kit.gl.vao::*vao-decl* (make-instance 'kit.gl.vao::vao-declaration)))
     (map nil
          (lambda (x)
            (kit.gl.vao::vao-add kit.gl.vao::*vao-decl*
                                 (kit.gl.vao::vao-parse x)))
-         (loop :for buffer-id :in (buffer-order spec)
-               :for attrs = (gethash buffer-id (layout-groups spec))
-               :nconc `((:interleave (:divisor 0) ,@attrs)))) ; TODO: add real group divisor
-    (setf (gethash (id spec) kit.gl.vao::*vao-declarations*)
+         (loop :for buffer-id :in (buffer-order layout)
+               :for attrs = (gethash buffer-id (group-attrs layout))
+               :for divisor = (gethash buffer-id (buffer-divisors layout))
+               :nconc `((:interleave (:divisor ,divisor) ,@attrs))))
+    (setf (gethash (id layout) kit.gl.vao::*vao-declarations*)
           kit.gl.vao::*vao-decl*)))
 
 (defmethod extension-file-type ((extension-type (eql 'vertex)))
   "vert")
 
 (defmethod prepare-extension ((extension-type (eql 'vertex)) owner path)
-  (let ((data (make-instance 'vertex-metadata)))
-    (with-slots (%layouts %specs) data
-      (setf %layouts (%collect-vertex-layouts path)
-            %specs (%collect-vertex-specs path %layouts)
-            (vertex-metadata owner) data))))
-
-(defun test (spec-name)
-  (loop :with spec = (gethash spec-name (specs first-light-example::*test*))
-        :for buffer-id :in (buffer-order spec)
-        :for attrs = (gethash buffer-id (layout-groups spec))
-        :nconc `((:interleave (:divisor 0) ,@attrs))))
+  (let* ((groups (%collect-vertex-groups path))
+         (layouts (%collect-vertex-layouts path groups)))
+    (setf (vertex-metadata owner)
+          (make-instance 'vertex-metadata :groups groups :layouts layouts))))
