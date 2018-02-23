19e6236711f3c81db85701195e88a30ea9a475d1
Michael Fiano <mail@michaelfiano.com>
2018-02-23 01:43:07 -0500

    Simplified shaders to debug easier

--- src/shaders/shader-stages.lisp
+++ src/shaders/shader-stages.lisp
@@ -6,65 +6,27 @@
   (sampler1 :sampler-2d :accessor sampler1)
   (sampler2 :sampler-2d :accessor sampler2))
 
-(defun-gpu default-vertex (;; These become vertex attributes
-                           (pos :vec3)
-                           (normal :vec3)
-                           (tangent :vec4)
-                           (color :vec4)
-                           (uv1 :vec2)
-                           (uv2 :vec2)
-                           (joints :vec4)
-                           (weights :vec4)
-                           ;; And these are uniforms.
-                           &uniform
-                           (model :mat4)
-                           (view :mat4)
-                           (proj :mat4))
-  (values
-   ;; position is implicitly first
-   (* proj view model (vec4 pos 1))
-   ;; Then the rest must match the fragment shader lambda-list
-   normal
-   tangent
-   color
-   uv1
-   uv2))
+(defun-gpu vert/default ((pos :vec3)
+                         (normal :vec3)
+                         (tangent :vec4)
+                         (color :vec4)
+                         (uv1 :vec2)
+                         (uv2 :vec2)
+                         (joints :vec4)
+                         (weights :vec4)
+                         &uniform
+                         (model :mat4)
+                         (view :mat4)
+                         (proj :mat4))
+  (values (* proj view model (vec4 pos 1))
+          uv1))
 
-(defun-gpu color-decal-fragment ((normal :vec3)
-                                 (tangent :vec4)
-                                 (color :vec4)
-                                 (uv1 :vec2)
-                                 (uv2 :vec2))
-  (if (= (.w color) 0)
-      (discard)
-      (values color)))
-
-(defun-gpu color-fragment ((normal :vec3)
-                           (tangent :vec4)
-                           (color :vec4)
-                           (uv1 :vec2)
-                           (uv2 :vec2))
-  (values color))
-
-(defun-gpu texture-fragment ((normal :vec3)
-                             (tangent :vec4)
-                             (color :vec4)
-                             (uv1 :vec2)
-                             (uv2 :vec2)
-                             &uniform
-                             (tex texture-struct))
-  (values (texture (sampler1 tex) uv1)))
-
-;; test enacting the shaders stuff.....
-
-(make-shader-program :unlit-color-decal (:version 430 :primitive :triangles)
-  (:vertex () (default-vertex :vec3 :vec3 :vec4 :vec4 :vec2 :vec2 :vec4 :vec4))
-  (:fragment () (color-decal-fragment :vec3 :vec4 :vec4 :vec2 :vec2)))
-
-(make-shader-program :unlit-color (:version 430 :primitive :triangles)
-  (:vertex () (default-vertex :vec3 :vec3 :vec4 :vec4 :vec2 :vec2 :vec4 :vec4))
-  (:fragment () (color-fragment :vec3 :vec4 :vec4 :vec2 :vec2)))
+(defun-gpu frag/texture ((uv :vec2) &uniform (tex texture-struct))
+  (let ((color (texture (sampler1 tex) uv)))
+    (if (= (.w color) 0)
+        (discard)
+        color)))
 
 (make-shader-program :texture (:version 430 :primitive :triangles)
-  (:vertex () (default-vertex :vec3 :vec3 :vec4 :vec4 :vec2 :vec2 :vec4 :vec4))
-  (:fragment () (texture-fragment :vec3 :vec4 :vec4 :vec2 :vec2)))
+  (:vertex () (vert/default :vec3 :vec3 :vec4 :vec4 :vec2 :vec2 :vec4 :vec4))
+  (:fragment () (frag/texture :vec2)))
