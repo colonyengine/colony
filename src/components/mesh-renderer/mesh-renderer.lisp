2c491e9d8c2c8977f6f7aadc72b8ae555c4e3fe1
Michael Fiano <mail@michaelfiano.com>
2018-02-23 01:43:49 -0500

    Cleaned up rendering. This fixed the bug of the display completely
    black when issuing a draw call with the new shader system. The problem was the draw call was not
    within the shader:with-shader-program form, and thus was trying to draw with glUseProgram equal to
    0. Moving a closing paren is all that was needed to end this hours long debugging session.

--- src/components/mesh-renderer/mesh-renderer.lisp
+++ src/components/mesh-renderer/mesh-renderer.lisp
@@ -27,44 +27,16 @@
                       texture-id (id cached)))))))))
 
 (defmethod render-component ((component mesh-renderer) (context context))
-  (with-accessors ((transform transform) (mesh mesh) (shader shader)
-                   (texture-id texture-id))
+  (with-accessors ((transform transform) (mesh mesh) (shader shader) (texture-id texture-id))
       component
-    (alexandria:when-let* ((shaders (shaders context))
-                           (camera (active-camera context)))
-
-      #++(progn
-           (kit.gl.shader:use-program shaders shader)
-           (uniform-matrix shaders :model (model transform))
-           (uniform-matrix shaders :view (view camera))
-           (uniform-matrix shaders :proj (projection camera))
-           (when texture-id
-             (gl:active-texture 0)
-             (gl:bind-texture :texture-2d texture-id)
-             (uniform-integer shaders :tex.sampler1 0)))
-
-      (progn
-        #++(format t "Attempting to render with shader: ~S (texture-id: ~A)~%"
-                   shader texture-id)
-        (shadow:with-shader-program shader
-          (shadow:uniform-mat4 :model (model transform))
-          (shadow:uniform-mat4 :view (view camera))
-          (shadow:uniform-mat4 :proj (projection camera))
-          (when texture-id
-            (gl:active-texture 0)
-            (gl:bind-texture :texture-2d texture-id)
-            (shadow:uniform-int :tex.sampler1 0))))
-      (dolist (primitive (primitives mesh))
-        (funcall (fl.assets:draw-func primitive))))))
-
-(defun uniform-matrix (shaders key value)
-  (kit.gl.shader:uniform-matrix-1-sv shaders key value))
-
-(defun uniform-vector (shaders key value)
-  (kit.gl.shader:uniformfv shaders key value))
-
-(defun uniform-integer (shaders key value)
-  (kit.gl.shader:uniformi shaders key value))
-
-(defun uniform-float (shaders key value)
-  (kit.gl.shader:uniformf shaders key value))
+    (alexandria:when-let* ((camera (active-camera context)))
+      (shadow:with-shader-program shader
+        (shadow:uniform-mat4 :model (model transform))
+        (shadow:uniform-mat4 :view (view camera))
+        (shadow:uniform-mat4 :proj (projection camera))
+        (when texture-id
+          (gl:active-texture 0)
+          (gl:bind-texture :texture-2d texture-id)
+          (shadow:uniform-int :tex.sampler1 0))
+        (dolist (primitive (primitives mesh))
+          (funcall (fl.assets:draw-func primitive)))))))
