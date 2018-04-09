2c27d95cbdf833438483d9aee8887fa6f64d313e
Peter Keller <psilord@cs.wisc.edu>
2018-04-09 05:03:29 -0500

    added more solid ideas for the texture-descriptor dsl and texture-parameter profiles

--- src/engine/texture.lisp
+++ src/engine/texture.lisp
@@ -83,3 +83,64 @@
 (defmethod rcache-unload ((entry-type (eql :texture)) (core-state core-state)
                           texture-location texture-id &key)
   (gl:delete-texture texture-id))
+
+
+
+;; TODO Not done.
+;; experimental texture descriptor work.
+(defmacro deftexdesc (&body body)
+  ;; TODO: Note make policy optional, set to :default-policy if not specified.
+  (multiple-value-bind (name target policy true-body)
+      (if (symbolp (first body))
+	  ;; maybe we can name the texture descriptor....
+	  (destructuring-bind (name (target policy) . true-body) body
+	    (values name target policy true-body))
+	  ;; or make it anonymous
+	  (destructuring-bind ((target policy) . true-body) body
+	    (values nil target policy true-body)))
+
+    ;; all variables are now valid.
+    `(progn
+       (list :name ',name :target ',target :policy ',policy :body ',true-body))))
+
+;; TODO Not done. define a texture parameter profile. MAybe rename.
+(defmacro deftexdesc-profile (&body args)
+  `(progn (list ',args)))
+
+
+;; Define a profile to provide defaults for texture parameters.
+(deftexdesc-profile default-profile
+  ;; texparameter stuff, opengl defaults
+  :depth-stencil-texture-mode :depth-component ;; note: ogl 4.3 or greater
+  :texture-base-level 0
+  :texture-border-color :todo-add-local-nicknames #++(v4:make 0.0 0.0 0.0 0.0)
+  :texture-compare-func :never ;; unknown default
+  :texture-compare-mode :none ;; unknown default
+  :texture-min-filter :nearest-mipmap-linear
+  :texture-mag-filter :linear
+  :texture-min-lod -1000
+  :texture-max-lod 1000
+  :texture-max-level 1000
+  :texture-swizzle-r :red
+  :texture-swizzle-g :green
+  :texture-swizzle-b :blue
+  :texture-swizzle-a :alpha
+  :texture-wrap-s :repeat
+  :texture_wrap-t :repeat
+  :texture-wrap-r :repeat)
+
+
+
+;; example texture descriptor, This one is named, 'name', probably
+;; fl.textures:name, but they can also be anonymous when used directly in the
+;; material DSL.  It is probably good that this be a real macro or function. I
+;; would change the syntax to make it easier to make a function. The materials
+;; DSL will evaluate the values for the uniforms, so this could be either.
+(deftexdesc name (:2d default-profile)
+  ;; the default-profile for texture parameters is what is used above, you can
+  ;; override it here.
+
+  :generate-mipmaps-p t ;; must only have a single base image. error otherwise
+  :flip-y t
+  :images #("path/to/texture.tga" ;; mipmap 0, or base image, always
+	    ))
