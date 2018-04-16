d6c8bae4a2f7c477c0b857d3a04105c908eef72f
Michael Fiano <mail@michaelfiano.com>
2018-04-16 11:02:47 -0400

    Cleanup shaders package.

--- src/shaders/package.lisp
+++ src/shaders/package.lisp
@@ -3,20 +3,15 @@
 (defpackage+ #:fl.shaders
   (:use #:cl #:shadow)
   (:inherit #:box.math.vari #:vari)
-  (:local-nicknames
-   (#:v2 #:box.math.vec2)
-   (#:v3 #:box.math.vec3)
-   (#:v4 #:box.math.vec4)
-   (#:m2 #:box.math.mat2)
-   (#:m3 #:box.math.mat3)
-   (#:m4 #:box.math.mat4)
-   (#:q #:box.math.quat))
-  (:export
-   ;; shader programs defined in *.shader-programs files that are in this
-   ;; package.
-   #:unlit-color
-   #:unlit-color-decal
-   #:unlit-texture
-   #:unlit-texture-decal
-   #:pbr-damaged-helmet
-   ))
+  (:local-nicknames (#:v2 #:box.math.vec2)
+                    (#:v3 #:box.math.vec3)
+                    (#:v4 #:box.math.vec4)
+                    (#:m2 #:box.math.mat2)
+                    (#:m3 #:box.math.mat3)
+                    (#:m4 #:box.math.mat4)
+                    (#:q #:box.math.quat))
+  (:export-only #:unlit-color
+                #:unlit-color-decal
+                #:unlit-texture
+                #:unlit-texture-decal
+                #:pbr-damaged-helmet))
