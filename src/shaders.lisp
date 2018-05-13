8ce554da8ae4328847b96798e540786827cc56ab
Peter Keller <psilord@cs.wisc.edu>
2018-05-13 00:24:05 -0500

    make core-state available to shaders-modified-hook.

--- src/shaders.lisp
+++ src/shaders.lisp
@@ -13,13 +13,16 @@
 (defmethod prepare-extension ((extension-type (eql 'shader-programs)) owner path)
   (load-extensions extension-type path))
 
-(defun shaders-modified-hook (programs-list)
-  (when t ;; this needs to be a guard to check if (display core-state) is non-nil
-    (sdl2:in-main-thread ()
-      (shadow:update-shader-programs programs-list))
-    (when programs-list
-      (simple-logger:emit :shader.programs.updated programs-list))))
+(defun gen-shaders-modified-hook (core-state)
+  (lambda (programs-list)
+    ;; NOTE: core-state is closed over and now available in the body of this
+    ;; returned function.
+    (when t ;; this needs to be a guard to check if (display core-state) is non-nil
+      (sdl2:in-main-thread ()
+        (shadow:update-shader-programs programs-list))
+      (when programs-list
+        (simple-logger:emit :shader.programs.updated programs-list)))))
 
 (defun prepare-shader-programs (core-state)
   (setf (shaders core-state) (shadow:build-shader-dictionary))
-  (shadow:set-modify-hook #'shaders-modified-hook))
+  (shadow:set-modify-hook (gen-shaders-modified-hook core-state)))
