dee61c521342cf5c301a92d465f3006c6e3ba589
Peter Keller <psilord@cs.wisc.edu>
2018-05-14 02:14:50 -0500

    Change the error on materials using unknown uniforms to explicit mention that implicit uniforms are not allowed in materials.

--- src/materials.lisp
+++ src/materials.lisp
@@ -295,7 +295,7 @@ etc. Return NIL otherwise."
                                        (vectorp thing))
                                    (copy-seq thing)
                                    thing))))))
-                  (error "Material ~s uses unknown uniform ~s in shader ~s."
+                  (error "Material ~s uses unknown uniform ~s in shader ~s. If it is an implicit uniform, those are not supported in materials."
                          (id material) uniform-name (id shader-program))))
 
    (uniforms material))
