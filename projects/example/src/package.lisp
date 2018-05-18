c3705a59e17f5f8c5f92e1621e6da1893c0c7b1b
Michael Fiano <mail@michaelfiano.com>
2018-05-18 03:03:47 -0400

    Started moving helmet to example project

--- projects/example/src/package.lisp
+++ projects/example/src/package.lisp
@@ -3,3 +3,16 @@
 (defpackage+ #:fl.example
   (:use #:cl #:fl.core)
   (:local-nicknames (#:v3 #:box.math.vec3)))
+
+(defpackage+ #:fl.example.shaders
+  (:use #:cl #:shadow #:box.math.vari))
+
+(defpackage+ #:fl.example.materials
+  (:use #:cl #:shadow)
+  (:import-from #:fl.core
+                #:define-material))
+
+(defpackage+ #:fl.example.textures
+  (:use #:cl #:shadow)
+  (:import-from #:fl.core
+                #:define-texture))
