16315476cbeefd7875a009126120a6c073039c2e
Michael Fiano <mail@michaelfiano.com>
2018-05-18 03:17:53 -0400

    It loads now!

--- projects/example/src/package.lisp
+++ projects/example/src/package.lisp
@@ -9,6 +9,12 @@
 
 (defpackage+ #:fl.example.materials
   (:use #:cl #:shadow)
+  (:local-nicknames (#:v2 #:box.math.vec2)
+                    (#:v3 #:box.math.vec3)
+                    (#:v4 #:box.math.vec4)
+                    (#:m2 #:box.math.mat2)
+                    (#:m3 #:box.math.mat3)
+                    (#:m4 #:box.math.mat4))
   (:import-from #:fl.core
                 #:define-material)
   (:export-only #:pbr-damaged-helmet))
