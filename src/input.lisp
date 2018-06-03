d26822a9cd1041bdf7dfc67576eb0503075d3f2e
Michael Fiano <mail@michaelfiano.com>
2018-06-03 07:53:06 -0400

    Allow key repeating

--- src/input.lisp
+++ src/input.lisp
@@ -2,10 +2,9 @@
 
 (defmethod kit.sdl2:keyboard-event ((display display) state ts repeatp keysym)
   (let ((key (sdl2:scancode keysym)))
-    (unless repeatp
-      (case state
-        (:keydown (key-down display key))
-        (:keyup (key-up display key))))))
+    (case state
+      (:keydown (key-down display key))
+      (:keyup (key-up display key)))))
 
 (defgeneric key-down (display key)
   (:method ((display display) key))
