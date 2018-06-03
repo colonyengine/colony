37c6ed1718870d6371bee2f9f6ecfb18123b7038
Michael Fiano <mail@michaelfiano.com>
2018-06-03 07:53:21 -0400

    Oops. added back mouse scrolling

--- projects/example/src/input.lisp
+++ projects/example/src/input.lisp
@@ -2,3 +2,11 @@
 
 (defmethod key-down ((display display) (key (eql :scancode-escape)))
   (stop-engine (core-state display)))
+
+(defmethod mouse-scroll-up ((display display))
+  (when (active-camera (context (core-state display)))
+    (fl.comp.camera:zoom-camera display 1)))
+
+(defmethod mouse-scroll-down ((display display))
+  (when (active-camera (context (core-state display)))
+    (fl.comp.camera:zoom-camera display -1)))
