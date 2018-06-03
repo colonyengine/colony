a66a1d771660f86ae4ac98fc0087952951570709
Michael Fiano <mail@michaelfiano.com>
2018-06-03 01:21:37 -0400

    Removed some input methods in the example project that didn't belong

--- projects/example/src/input.lisp
+++ projects/example/src/input.lisp
@@ -2,9 +2,3 @@
 
 (defmethod key-down ((display display) (key (eql :scancode-escape)))
   (stop-engine (core-state display)))
-
-(defmethod mouse-scroll-up ((display display))
-  (fl.comp.camera:zoom-camera display 1))
-
-(defmethod mouse-scroll-down ((display display))
-  (fl.comp.camera:zoom-camera display -1))
