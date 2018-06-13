8025c17c87714d387e1f6c2106c39bb8d1add60e
Michael Fiano <mail@michaelfiano.com>
2018-06-13 18:06:25 -0400

    Modified gamepad database update script

--- tools/update-gamepad-db.sh
+++ tools/update-gamepad-db.sh
@@ -5,4 +5,6 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" ; pwd -P)"
 REMOTE_FILE="https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt"
 LOCAL_FILE="${SCRIPT_PATH}/../data/gamepad-db.txt"
 
+echo -n "Updating gamepad database ... "
 wget -q ${REMOTE_FILE} -O ${LOCAL_FILE}
+echo "DONE"
