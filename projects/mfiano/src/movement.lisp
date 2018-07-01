1e4d4adcbc1b5eae435f78424208027907f63532
Peter Keller <psilord@cs.wisc.edu>
2018-07-01 17:22:03 -0500

    Fix the codes to emit shots like how one expects. There is a huge TODO in here which must be resolved in gamebox-math. Basically quat:from-mat4 needs a keyword argument to notify it if it should make a copy of the basis vectors, then normalize them, and then use those copies in the algorithm. Basically, quat:from-mat4's conversion algorithm EXPECTS normalized basis vectors, but it is most often the case in FL that they won't be. So, by default it should temporarily normalize the basis vectors it uses for its own conversion routine.
    I also changed the angle the atan produces out into a rand of 0 <=
    angle < 2pi for easier debugging of the rotation matricies of the
    ship. There was nothing wrong with it mathematically, except it
    was hard to see the bouncing of the basis vector values across the
    boundary when debugging.
    
    Changed the fire button to :right-shoulder since it is too hard to use :a
    when actually controlling the joysticks.

--- projects/mfiano/src/movement.lisp
+++ projects/mfiano/src/movement.lisp
@@ -15,7 +15,11 @@
         (v3:scale! vec (if (> (v3:magnitude vec) 1) (v3:normalize vec) vec) 150.0)
         (fl.comp:translate %transform (v3:+ (v3:make -400 0 0) vec) :replace-p t))
       (unless (= rx ry 0.0)
-        (let ((angle (atan (- rx) ry)))
+        (let* ((angle (atan (- rx) ry))
+               ;; keep angle from 0 to 2pi for easier debugging of other things.
+               (angle (if (< angle 0)
+                          (+ pi (- pi (abs angle)))
+                          angle)))
           (fl.comp:rotate %transform (v3:make 0 0 angle) :replace-p t))))))
 
 
@@ -25,45 +29,86 @@
 
 (define-component shot-mover ()
   ((transform :default nil)
-   (velocity :default (v3:make 0 0 0))))
+   (velocity :default 0)))
 
 (defmethod initialize-component ((component shot-mover) (context context))
   (setf (transform component)
         (actor-component-by-type (actor component) 'transform)))
 
+
 (defmethod update-component ((component shot-mover) (context context))
-  (fl.comp:translate (transform component) (velocity component)))
+  (fl.comp:translate
+   (transform component)
+   ;; fly along the forward axis for this shot
+   (let* ((a (m4:rotation-axis-to-vec3
+              (fl.comp:local (transform component)) :y))
+          (b  (v3:normalize a))
+          (move-delta (* (velocity component) (frame-time context))))
+     (v3:scale b move-delta))))
 
 
+(define-component shot-emitter ()
+  ((emitter-transform :default nil)))
 
-
-(define-component shot-emitter ())
-
-(defmethod initialize-component ((component shot-emitter) (context context)))
+(defmethod initialize-component ((component shot-emitter) (context context))
+  (setf (emitter-transform component)
+        (actor-component-by-type (actor component) 'transform)))
 
 (defmethod update-component ((component shot-emitter) (context context))
-  (when (or (input-enter-p context '(:gamepad1 :a))
+  (when (or (input-enter-p context '(:gamepad1 :right-shoulder))
             (input-enter-p context '(:mouse :mouse-left)))
 
-    (let ((actor (%fl::make-actor context :id (au:unique-name 'shot)))
-          (transform (make-component 'fl.comp:transform context))
-          (shot-mover (make-component
-                       'shot-mover context
-                       :velocity (v3:make 0 (* 1000 (frame-time context)) 0)))
-          (sprite (make-component 'sprite-sheet context
-                                  :spec-path '(:local "data/sprites.sexp")
-                                  :material 'fl.mfiano.materials::sprite
-                                  :animations (make-sprite-sheet-animations
-                                               0 0 #(#(.25
-                                                       "bullet01"
-                                                       "bullet02"
-                                                       ))))))
+    (let* ((parent-model (fl.comp:model (emitter-transform component)))
+           (parent-translation (m4:translation-to-vec3 parent-model))
+           (parent-rotation
+             ;; TODO: The model matrix has scaling on it, which fucks up the
+             ;; quat conversion algorithm. So we test getting rid of it here.
+             ;;
+             ;; This must be fixed by going into gamebox-math/src/quat.lisp and
+             ;; adding a "&key (normalize t)" argument to the function.  Then,
+             ;; when normalize is t (the default), it temporarily normalizes the
+             ;; basis vectors and uses those instead. Then the from-quat4
+             ;; conversion will work regardless of scaling on the transform.
+             (let* ((x-rot (m4:rotation-axis-to-vec3 parent-model :x))
+                    (x-rot (v3:normalize! x-rot x-rot))
+                    (y-rot (m4:rotation-axis-to-vec3 parent-model :y))
+                    (y-rot (v3:normalize! y-rot y-rot))
+                    (z-rot (m4:rotation-axis-to-vec3 parent-model :z))
+                    (z-rot (v3:normalize! z-rot z-rot))
+                    (unscaled-mat (m4:id)))
+               (m4:rotation-axis-from-vec3! unscaled-mat x-rot :x)
+               (m4:rotation-axis-from-vec3! unscaled-mat y-rot :y)
+               (m4:rotation-axis-from-vec3! unscaled-mat z-rot :z)
+               ;; finally get the rotation.
+               ;; with the above suggested fix to gamebox-math, this call will
+               ;; just do the right thing and can be replaced with
+               ;; (quat:from-mat4 parent-model)
+               (quat:from-mat4 unscaled-mat)))
 
-      (attach-multiple-components actor transform shot-mover sprite)
+           ;; here is the code to make the new shot.
+           (new-actor (%fl::make-actor context :id (au:unique-name 'shot)))
+           (transform (make-component 'fl.comp:transform context
+                                      ;; here I init the local location/rotation
+                                      ;; to semantically match the emitter
+                                      ;; actor.
+                                      :translation/current parent-translation
+                                      :rotation/current parent-rotation))
+           (shot-mover (make-component 'shot-mover context
+                                       :velocity 1000))
+           (sprite (make-component 'sprite-sheet context
+                                   :spec-path '(:local "data/sprites.sexp")
+                                   :material 'fl.mfiano.materials::sprite
+                                   :animations (make-sprite-sheet-animations
+                                                0 0 #(#(.25
+                                                        "bullet01"
+                                                        "bullet02"
+                                                        ))))))
 
-      (spawn-actor actor context :parent (actor component))
+      (attach-multiple-components new-actor transform shot-mover sprite)
 
+      ;; The shot is free in the universe.
+      (spawn-actor new-actor context)
 
       ;; This is the method for destroying actors and components. Add to public
       ;; API. Don't use :ttl in the make-actor call yet.
-      (%fl::destroy actor context :ttl 1))))
+      (%fl::destroy new-actor context :ttl 1))))
