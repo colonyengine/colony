69d5582dc438f9be436c9b642b4f1df0029cb5e2
Peter Keller <psilord@cs.wisc.edu>
2018-12-12 14:47:56 -0600

    Preparing for cube-map-arrays.

--- core/texture.lisp
+++ core/texture.lisp
@@ -34,7 +34,7 @@
 ;; TODO: Candidate for public API
 (defun add-semantic-texture-descriptor (texdesc core-state)
   (setf (fl.util:href (semantic-texture-descriptors (textures core-state))
-                 (name texdesc))
+                      (name texdesc))
         texdesc))
 
 ;; TODO: Candidate for public API
@@ -42,8 +42,8 @@
   "Add a TEXTURE, which only has defined an opengl texid, to core-state. This
 book keeps unrealized textures for the user to finalize before the game starts."
   (setf (fl.util:href (unrealized-procedural-textures
-                  (textures (core-state context)))
-                 (name texture))
+                       (textures (core-state context)))
+                      (name texture))
         texture))
 
 ;; TODO: Candidate for public API
@@ -67,7 +67,7 @@ NOTE: These are already in the RCACHE."
   "Return a list of all procedural texture descriptors."
   (let ((results ()))
     (fl.util:do-hash-values (texdesc (semantic-texture-descriptors
-                                 (textures (core-state context))))
+                                      (textures (core-state context))))
       (when (eq (texture-type texdesc) :procedural)
         (push texdesc results)))
     (nreverse results)))
@@ -76,7 +76,7 @@ NOTE: These are already in the RCACHE."
 ;; TODO: Candidate for public API
 (defun find-semantic-texture-descriptor (semantic-texture-name context)
   (fl.util:href (semantic-texture-descriptors (textures (core-state context)))
-           semantic-texture-name))
+                semantic-texture-name))
 
 ;; TODO: Candidate for public API
 (defclass texture-profile ()
@@ -170,7 +170,7 @@ NOTE: These are already in the RCACHE."
 
 (defun (setf get-semantic-applied-attribute) (newval texture attribute-name)
   (setf (fl.util:href (applied-attributes (semantic-texdesc texture))
-                 attribute-name)
+                      attribute-name)
         newval))
 
 (defun get-computed-applied-attribute (texture attribute-name)
@@ -178,7 +178,7 @@ NOTE: These are already in the RCACHE."
 
 (defun (setf get-computed-applied-attribute) (newval texture attribute-name)
   (setf (fl.util:href (applied-attributes (computed-texdesc texture))
-                 attribute-name)
+                      attribute-name)
         newval))
 
 ;; TODO: Candidate public API
@@ -959,8 +959,91 @@ and return it."
 
 
 (defmethod load-texture-data ((texture-type (eql :texture-cube-map-array)) texture context)
-  (error "load-texture-data: :texture-cube-map-array implement me")
-  nil)
+  (let* ((use-mipmaps-p (get-computed-applied-attribute texture :use-mipmaps))
+         (immutable-p (get-computed-applied-attribute texture :immutable))
+         (texture-max-level
+           (get-computed-applied-attribute texture :texture-max-level))
+         (texture-base-level
+           (get-computed-applied-attribute texture :texture-base-level))
+         (max-mipmaps (- texture-max-level texture-base-level))
+         (data (get-computed-applied-attribute texture :data)))
+
+    ;; load all of the images we may require.
+    (let* ((images (read-mipmap-images context data use-mipmaps-p
+                                       :cube-map-array))
+           (first-cube (aref images 0))
+           (first-image (aref (second (aref first-cube 0)) 0))
+           ;; TODO: This is not safe, need to check all of them.
+           (num-mipmaps (length (second (aref first-cube 0)))))
+
+      (v:trace :fl.core.texture "Loading :texture-cube-map-array images = ~a"
+               images)
+
+      ;; Check to ensure they all fit into texture memory.
+      ;;
+      ;; TODO: Refactor out of each method into validate-mipmap-images and
+      ;; generalize.
+      (loop :for (placement mipmaps) :across first-cube
+            :for image = (aref mipmaps 0)
+            :do (when (> (max (height image) (width image))
+                         (gl:get-integer :max-texture-size))
+                  ;; TODO: print out the location of the failing image.
+                  (error "An Image for texture ~A is to big to be loaded onto this card. Max resolution is ~A in either dimension."
+                         (name texture)
+                         (gl:get-integer :max-texture-size))))
+
+
+      ;; Figure out the ideal mipmap count from the base resolution.
+      (multiple-value-bind (expected-mipmaps expected-resolutions)
+          ;; TODO: This might need work with cube-maps.
+          (compute-mipmap-levels (width first-image)
+                                 (height first-image))
+        (declare (ignore expected-resolutions))
+
+
+        ;; TODO: Fix this up for cube-maps
+        #++(validate-mipmap-images images texture
+                                   expected-mipmaps expected-resolutions)
+
+        (potentially-degrade-texture-min-filter texture)
+
+        ;; Allocate immutable storage if required.
+        (when immutable-p
+          (let ((num-mipmaps-to-generate
+                  (if use-mipmaps-p (min expected-mipmaps max-mipmaps) 1)))
+            (%gl:tex-storage-2d texture-type num-mipmaps-to-generate
+                                (internal-format first-image)
+                                (width first-image)
+                                (height first-image))))
+
+        ;; Insert all cubes plus mipmaps into the GPU.
+        (loop
+          :for cube :across images
+          :do
+             (loop
+               :for idx :below (if use-mipmaps-p num-mipmaps 1)
+               :for level = (+ texture-base-level idx)
+               ;; Now load all six faces, each face at the same mipmap level.
+               :do
+                  (loop
+                    :for (face-signifier mipmaps) :across cube
+                    :do
+                       (with-slots (%width %height %internal-format
+                                    %pixel-format %pixel-type %data)
+                           (aref mipmaps idx)
+                         (if immutable-p
+                             (gl:tex-sub-image-2d face-signifier level 0 0
+                                                  %width %height
+                                                  %pixel-format
+                                                  %pixel-type %data)
+                             (gl:tex-image-2d face-signifier level
+                                              %internal-format
+                                              %width %height 0
+                                              %pixel-format %pixel-type
+                                              %data))))))
+
+        (free-mipmap-images images :cube-map-array)
+        (potentially-autogenerate-mipmaps texture-type texture)))))
 
 (defmethod load-texture-data ((texture-type (eql :texture-rectangle)) texture context)
   (error "load-texture-data: :texture-rectangle implement me")
