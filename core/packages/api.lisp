47145762ae4f72d7c87761ccb0120add11bf00f7
Michael Fiano <mail@michaelfiano.com>
2018-12-17 21:28:46 -0500

    Only export what we need. More can be added later

--- core/packages/api.lisp
+++ core/packages/api.lisp
@@ -3,79 +3,45 @@
 (defpackage+ #:first-light
   (:nicknames #:fl)
   (:use #:cl)
-  ;; Deplying binaries
-  (:inherit-from #:%first-light
-                 #:deploy-binary)
-  ;; common
-  (:inherit-from #:%first-light
-                 #:define-options
-                 #:id
-                 #:option
-                 #:start-engine
-                 #:stop-engine
-                 #:with-shared-storage)
-  ;; resources
-  (:inherit-from #:%first-light
-                 #:define-resources
-                 #:find-resource
-                 #:print-all-resources)
-  ;; extensions
-  (:inherit-from #:%first-light
-                 #:extension-file-type
-                 #:prepare-extension)
-  ;; context
   (:inherit-from #:%first-light
                  #:active-camera
-                 #:context
-                 #:project-data
-                 #:delta
-                 #:epilogue
-                 #:frame-time
-                 #:input-data
-                 #:prologue
-                 #:ss-href
-                 #:total-time)
-  ;; textures
-  (:inherit-from #:%first-light
-                 #:define-texture
-                 #:define-texture-profile
-                 #:general-data-format-descriptor)
-  ;; materials
-  (:inherit-from #:%first-light
-                 #:bind-material
-                 #:copy-material
-                 #:define-material
-                 #:define-material-profile
-                 #:lookup-material
-                 #:mat-computed-uniform-ref
-                 #:mat-uniform-ref
-                 #:shader
-                 #:using-material)
-  ;; shaders
-  (:inherit-from #:%first-light
-                 #:define-shader)
-  ;; scene
-  (:inherit-from #:%first-light
-                 #:define-scene)
-  ;; actor
-  ;; TODO: Finish user API.
-  (:inherit-from #:%first-light
                  #:actor
                  #:actor-component-by-type
                  #:actor-components-by-type
                  #:attach-component
                  #:attach-multiple-components
-                 #:detach-component
-                 #:make-actor
-                 #:spawn-actor)
-  ;; components
-  ;; TODO: Finish user API.
-  (:inherit-from #:%first-light
+                 #:context
+                 #:delta
+                 #:deploy-binary
                  #:define-component
+                 #:define-material
+                 #:define-material-profile
+                 #:define-options
+                 #:define-resources
+                 #:define-scene
+                 #:define-texture
+                 #:define-texture-profile
+                 #:detach-component
                  #:destroy-component
+                 #:extension-file-type
+                 #:find-resource
+                 #:frame-time
+                 #:general-data-format-descriptor
                  #:initialize-component
+                 #:input-data
+                 #:make-actor
                  #:make-component
-                 #:physics-update-component
+                 #:mat-uniform-ref
+                 #:option
+                 #:prepare-extension
+                 #:print-all-resources
+                 #:project-data
                  #:render-component
-                 #:shared-storage-metadata
-                 #:update-component))
+                 #:spawn-actor
+                 #:ss-href
+                 #:start-engine
+                 #:stop-engine
+                 #:total-time
+                 #:update-component
+                 #:using-material
+                 #:with-shared-storage))
--- core/packages/internal.lisp
+++ core/packages/internal.lisp
@@ -3,84 +3,55 @@
 (defpackage+ #:%first-light
   (:nicknames #:%fl)
   (:use #:cl)
-  ;; common
-  (:export #:cameras
-           #:core-state
-           #:define-options
-           #:display
-           #:frame-manager
-           #:id
-           #:option
-           #:rcache
-           #:rcache-construct
-           #:rcache-dispose
-           #:rcache-lookup
-           #:rcache-remove
-           #:scene-tree
-           #:shared-storage
-           #:start-engine
-           #:stop-engine
-           #:window
-           #:with-shared-storage)
-  ;; resources
-  (:export #:define-resources
-           #:find-resource
-           #:print-all-resources)
-  ;; extensions
-  (:export #:extension-file-type
-           #:prepare-extension)
-  ;; context
-  (:export #:active-camera
-           #:context
-           #:project-data
-           #:delta
-           #:epilogue
-           #:frame-time
-           #:input-data
-           #:prologue
-           #:shared-storage-table
-           #:ss-href
-           #:total-time)
-  ;; textures
-  (:export #:define-texture
-           #:define-texture-profile
-           #:general-data-format-descriptor
-           #:load-texture)
-  ;; materials
-  (:export #:bind-material
-           #:copy-material
-           #:define-material
-           #:define-material-profile
-           #:lookup-material
-           #:mat-computed-uniform-ref
-           #:mat-uniform-ref
-           #:shader
-           #:using-material)
-  ;; shaders
-  (:export #:define-shader)
-  ;; scene
-  (:export #:define-scene)
-  ;; actor
-  (:export #:actor
-           #:attach-component
-           #:attach-multiple-components
-           #:detach-component
-           #:make-actor
-           #:spawn-actor)
-  ;; components
-  (:export #:actor-component-by-type
-           #:actor-components-by-type
-           #:component
-           #:define-component
-           #:destroy-component
-           #:initialize-component
-           #:make-component
-           #:physics-update-component
-           #:render-component
-           #:shared-storage-metadata
-           #:state
-           #:update-component)
-  ;; annotation API
-  (:export #:define-annotation)
-  ;; deploying binaries
-  (:export #:deploy-binary))
+  (:export
+   #:active-camera
+   #:actor
+   #:actor-component-by-type
+   #:actor-components-by-type
+   #:attach-component
+   #:attach-multiple-components
+   #:cameras
+   #:component
+   #:context
+   #:copy-material
+   #:core-state
+   #:define-annotation
+   #:define-component
+   #:define-material
+   #:define-material-profile
+   #:define-options
+   #:define-resources
+   #:define-scene
+   #:define-texture
+   #:define-texture-profile
+   #:delta
+   #:deploy-binary
+   #:destroy-component
+   #:detach-component
+   #:extension-file-type
+   #:find-resource
+   #:frame-manager
+   #:frame-time
+   #:general-data-format-descriptor
+   #:initialize-component
+   #:input-data
+   #:lookup-material
+   #:make-actor
+   #:make-component
+   #:mat-uniform-ref
+   #:option
+   #:prepare-extension
+   #:print-all-resources
+   #:project-data
+   #:render-component
+   #:scene-tree
+   #:shader
+   #:spawn-actor
+   #:ss-href
+   #:start-engine
+   #:state
+   #:stop-engine
+   #:total-time
+   #:update-component
+   #:using-material
+   #:with-shared-storage))
