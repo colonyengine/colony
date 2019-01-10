949b1262e66265a8fbc3a367bba74277ee5c9fe4
Michael Fiano <mail@michaelfiano.com>
2019-01-09 23:55:27 -0500

    Removed documentation about `define-component` in the mesh definition.

--- core/components/mesh.lisp
+++ core/components/mesh.lisp
@@ -1,21 +1,10 @@
 (in-package :first-light.components)
 
 (define-component mesh ()
-  ;; Component slot descriptions all in one form, like defclass
   ((location :default nil)
    (id :default 0)
    (primitives :default nil))
-
-  ;; Shared storage namespace definitions. First form is a namespace symbol
-  ;; which could be in a different package if desired, the &rest is a
-  ;; sequence of hash table test functions that will be consulted when
-  ;; making nested hash tables.
-  (;; Have we loaded this mesh before.
-   ;; Key Path: location[a cons], id[an integer]
-   ;; Value: loaded mesh data
-   ;; NOTE the tests must only be EQ EQL EQUAL and EQUALP and must be the
-   ;; raw symbols, no quotes or #' or (function eq) allowed.
-   (:cached-mesh-data equalp eql)))
+  ((:cached-mesh-data equalp eql)))
 
 (defun draw-mesh (mesh &optional count)
   (dolist (primitive (primitives mesh))
