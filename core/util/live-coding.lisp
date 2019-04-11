166c378859a5da48058018dc53a9265be5cbf934
Michael Fiano <mail@michaelfiano.com>
2019-04-11 12:07:05 -0400

    Fix live-coding support on SLIME.

--- core/util/live-coding.lisp
+++ core/util/live-coding.lisp
@@ -18,33 +18,33 @@
              (setf %pause-time (- (get-time) debugger-entry-time))))))))
 
 (defun compile-live-coding-functions ()
-  (au:if-let ((repl-package (find-if #'find-package '(:slynk :swank))))
-    (progn
-      (compile 'find-lisp-repl
-               `(lambda ()
-                  (when ,repl-package
-                    (load-time-value
-                     (or ,(au:ensure-symbol "*EMACS-CONNECTION*" repl-package)
-                         (,(au:ensure-symbol "DEFAULT-CONNECTION"
-                                             repl-package)))))))
-      (compile 'setup-lisp-repl
-               `(lambda ()
-                  (when ,(eq repl-package :slynk)
-                    (,(au:ensure-symbol "SEND-PROMPT" "SLYNK-MREPL")
-                     (find (bt:current-thread)
-                           (,(au:ensure-symbol "CHANNELS" repl-package))
-                           :key #',(au:ensure-symbol "CHANNEL-THREAD"
-                                                     repl-package))))))
-      (compile 'update-lisp-repl
-               `(lambda ()
-                  (when ,repl-package
-                    (au:when-let ((repl (find-lisp-repl)))
-                      (with-continue-restart "REPL"
-                        (,(au:ensure-symbol "HANDLE-REQUESTS" repl-package)
-                         repl t)))))))
-    (progn
-      (compile 'setup-lisp-repl (constantly nil))
-      (compile 'update-lisp-repl (constantly nil)))))
+  (let ((repl-package (find-if #'find-package '(:slynk :swank))))
+    (case repl-package
+      ((:slynk :swank)
+       (compile 'find-lisp-repl
+                `(lambda ()
+                   (or ,(au:ensure-symbol "*EMACS-CONNECTION*" repl-package)
+                       (,(au:ensure-symbol "DEFAULT-CONNECTION"
+                                           repl-package)))))
+       (compile 'setup-lisp-repl
+                (ecase repl-package
+                  (:slynk
+                   `(lambda ()
+                      (,(au:ensure-symbol "SEND-PROMPT" "SLYNK-MREPL")
+                       (find (bt:current-thread)
+                             (,(au:ensure-symbol "CHANNELS" repl-package))
+                             :key #',(au:ensure-symbol "CHANNEL-THREAD"
+                                                       repl-package)))))
+                  (:swank
+                   (constantly nil))))
+       (compile 'update-lisp-repl
+                `(lambda ()
+                   (au:when-let ((repl (find-lisp-repl)))
+                     (with-continue-restart "REPL"
+                       (,(au:ensure-symbol "HANDLE-REQUESTS" repl-package)
+                        repl t))))))
+      (t (setf (symbol-function 'setup-lisp-repl) (constantly nil)
+               (symbol-function 'update-lisp-repl) (constantly nil))))))
 
 (defun setup-live-coding ()
   (compile-live-coding-functions)
