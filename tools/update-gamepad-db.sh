d6cd5d0e13311559cae931c0461914283bd9da90
Peter Keller <psilord@cs.wisc.edu>
2019-05-27 01:30:12 -0500

    Update the gamepad-db.txt file.
    Fix the script to put the file into the right place since it moved
    eariler.
    Fix the script to add custom definitions. Currently there are none, but
    only because the canonical version JUST made a new release that added by
    default the one I was just about to put in here. So might as well just
    keep the code I wrote.

--- tools/update-gamepad-db.sh
+++ tools/update-gamepad-db.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 # Download and install the gamepad mapping database.
 
+set -e
+
 SCRIPT_PATH="$(cd "$(dirname "$0")" ; pwd -P)"
 REMOTE_FILE="https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt"
-LOCAL_FILE="${SCRIPT_PATH}/../data/gamepad-db.txt"
+LOCAL_FILE="${SCRIPT_PATH}/../data/core/gamepad-db.txt"
+CUSTOM_FILE="${SCRIPT_PATH}/custom-gamepad-db.txt"
 
 echo -n "Updating gamepad database ... "
 wget -q ${REMOTE_FILE} -O ${LOCAL_FILE}
 echo "DONE"
+
+echo -n "Appending custom definitions ... "
+cat < ${CUSTOM_FILE} >> ${LOCAL_FILE}
+echo "DONE"
+
+exit 0
