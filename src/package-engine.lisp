c04cd7c82d92a2e16e3f03a07f05a48e2fcf7c6b
Michael Fiano <mail@michaelfiano.com>
2019-08-04 14:33:56 -0400

    simplified engine package definition

--- src/package-engine.lisp
+++ src/package-engine.lisp
@@ -1,47 +1,10 @@
 (in-package #:cl-user)
 
-(defpackage #:virality.engine
+(uiop:define-package #:virality.engine
   (:use #:cl)
-  (:import-from #:virality.geometry
-                #:define-geometry
-                #:define-geometry-layout)
-  (:import-from #:virality.input
-                #:get-gamepad-analog
-                #:get-mouse-position
-                #:get-window-mode
-                #:get-window-size
-                #:get-window-title
-                #:input-enabled-p
-                #:input-enter-p
-                #:input-exit-p
-                #:on-gamepad-analog-move
-                #:on-gamepad-attach
-                #:on-gamepad-button-down
-                #:on-gamepad-button-up
-                #:on-gamepad-detach
-                #:on-window-close
-                #:on-window-hide
-                #:on-window-maximize
-                #:on-window-minimize
-                #:on-window-keyboard-focus-enter
-                #:on-window-keyboard-focus-exit
-                #:on-window-mouse-focus-enter
-                #:on-window-mouse-focus-exit
-                #:on-window-move
-                #:on-window-resize
-                #:on-window-restore
-                #:on-window-show
-                #:set-window-hidden
-                #:set-window-mode
-                #:set-window-title
-                #:set-window-size
-                #:set-window-visible)
-  (:import-from #:virality.prefabs
-                #:define-prefab
-                #:define-prefab-descriptor
-                #:prefab-descriptor
-                #:make-prefab-instance
-                #:ref)
+  (:mix-reexport #:virality.geometry
+                 #:virality.input
+                 #:virality.prefabs)
 
   ;; common
   (:export
@@ -105,59 +68,16 @@
    #:define-annotation
    #:define-call-flow
    #:define-component
-   #:define-geometry
-   #:define-geometry-layout
    #:define-graph
    #:define-material
    #:define-material-profile
    #:define-options
-   #:define-prefab
-   #:define-prefab-descriptor
    #:define-resources
    #:define-texture
    #:define-texture-profile)
-
-  ;; input
-  (:export
-   #:get-gamepad-analog
-   #:get-mouse-position
-   #:get-window-mode
-   #:get-window-size
-   #:get-window-title
-   #:input-enabled-p
-   #:input-enter-p
-   #:input-exit-p
-   #:on-gamepad-analog-move
-   #:on-gamepad-attach
-   #:on-gamepad-button-down
-   #:on-gamepad-button-up
-   #:on-gamepad-detach
-   #:on-window-close
-   #:on-window-hide
-   #:on-window-maximize
-   #:on-window-minimize
-   #:on-window-keyboard-focus-enter
-   #:on-window-keyboard-focus-exit
-   #:on-window-mouse-focus-enter
-   #:on-window-mouse-focus-exit
-   #:on-window-move
-   #:on-window-resize
-   #:on-window-restore
-   #:on-window-show
-   #:set-window-hidden
-   #:set-window-mode
-   #:set-window-title
-   #:set-window-size
-   #:set-window-visible)
 
   ;; materials
   (:export
    #:copy-material
    #:mat-uniform-ref
-   #:with-material)
-
-  ;; prefabs
-  (:export
-   #:make-prefab-instance
-   #:prefab-descriptor
-   #:ref))
+   #:with-material))
