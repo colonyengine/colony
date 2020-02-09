254c6efb896a45e8748c88b1be1affbee3839703
Peter Keller <psilord@cs.wisc.edu>
2020-02-08 21:15:34 -0600

    Updated collistion-test-3 to show a close detail about oob/oob.

--- examples/example-collision.lisp
+++ examples/example-collision.lisp
@@ -431,24 +431,49 @@ actually are. You have to view the results to see the colliders lighting up."
   (("camera" :copy "/cameras/ortho")
    (c/cam:camera (:policy :new-args) :zoom 140f0))
 
-  ("a"
-   ;; FIXME: In the below -0.50f0 causes a hit, since it is less than 1 away
-   ;; from the other collider. Changing this to -0.51f0 does NOT cause a hit,
-   ;; even though it is clearly penetrating. This seems to be related to the
-   ;; extents of the cube, because changing the min/max's to [-1, 1] instead of
-   ;; [-0.5, 0.5] causes a miss by a length of 2 instead of 1.
-   (c/xform:transform :translate (v3:vec -.50f0 0f0 0f0)
-                      :translate/inc (v3:vec 0f0 0f0 0f0))
-   ("cuboid1"
-    (c/xform:transform :rotate (q:orient :local :z o:pi/4))
+  ("test-case-always"
+   (c/xform:transform :translate (v3:vec -2f0 0f0 0f0))
+   ("a"
+    ;; As cuboid 1 rotates it should always be hitting (red) cuboid 2 since they
+    ;; penetrate and then share a plane when both are parallel to each other.
+    (c/xform:transform :translate (v3:vec -.50f0 0f0 0f0)
+                       :translate/inc (v3:vec 0f0 0f0 0f0))
+    ("cuboid1"
+     (c/xform:transform :rotate (q:orient :local :z o:pi/4)
+                        :rotate/inc (o:make-velocity (v3:vec 0 0 1) o:pi/12))
+     (c/col:cuboid :visualize t
+                   :on-layer :ground
+                   :center (v3:vec))))
+   ("cuboid2"
+    (c/xform:transform :translate (v3:vec 0.5f0 0f0 0f0))
     (c/col:cuboid :visualize t
                   :on-layer :ground
                   :center (v3:vec))))
-  ("cuboid2"
-   (c/xform:transform :translate (v3:vec 0.5f0 0f0 0f0))
-   (c/col:cuboid :visualize t
-                 :on-layer :ground
-                 :center (v3:vec))))
+
+  ("test-case-gap"
+   (c/xform:transform :translate (v3:vec 2f0 0f0 0f0))
+   ("a"
+    ;; As cuboid 1 rotates, when they become parallel, there will be a slight
+    ;; gap and so both should turn green to represent no collision during that
+    ;; small gap.
+    (c/xform:transform :translate (v3:vec -.51f0 0f0 0f0)
+                       :translate/inc (v3:vec 0f0 0f0 0f0))
+    ("cuboid1"
+     (c/xform:transform :rotate (q:orient :local :z o:pi/4)
+                        :rotate/inc (o:make-velocity (v3:vec 0 0 1) o:pi/12))
+     (c/col:cuboid :visualize t
+                   :on-layer :ground
+                   :center (v3:vec))))
+   ("cuboid2"
+    (c/xform:transform :translate (v3:vec 0.5f0 0f0 0f0))
+    (c/col:cuboid :visualize t
+                  :on-layer :ground
+                  :center (v3:vec)))))
+
+
+
+
+
 
 ;;; Prefab descriptors
 
