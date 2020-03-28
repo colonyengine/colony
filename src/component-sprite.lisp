6a8615e7383c2388cddd163b30c9eeb438e19a83
Peter Keller <psilord@cs.wisc.edu>
2020-03-28 18:11:28 -0500

    Shadow changed its API. Update Virality to reflect that.

--- src/component-sprite.lisp
+++ src/component-sprite.lisp
@@ -20,7 +20,8 @@
           :initarg :spec)
    (%geometry :reader geometry
               :initarg :geometry)
-   (%buffer :reader buffer)
+   (%buffer-name :reader buffer-name)
+   (%buffer-id :reader buffer-id)
    (%sprites :reader sprites
              :initform (u:dict #'equalp))))
 
@@ -34,8 +35,8 @@
       spritesheet)))
 
 (defun write-spritesheet-buffer (spritesheet)
-  (with-slots (%spec %buffer %sprites) spritesheet
-    (loop :with buffer-name = (gpu:buffer-name %buffer)
+  (with-slots (%spec %buffer-name %sprites) spritesheet
+    (loop :with buffer-name = %buffer-name
           :with count = (length %spec)
           :with pos = (make-array count)
           :with size = (make-array count)
@@ -50,15 +51,15 @@
                    (gpu:write-buffer-path buffer-name :size size))))
 
 (defun make-spritesheet-buffer (spritesheet)
-  (with-slots (%spec %buffer) spritesheet
+  (with-slots (%spec %buffer-name %buffer-id) spritesheet
     ;; TODO: This 1 is hardcoded because virality doesn't have an allocation
     ;; system for these integers.
     (gpu:bind-block :spritesheet 1)
-    (setf %buffer (gpu:create-buffer (a:make-gensym :spritesheet)
-                                     :spritesheet))
+    (setf %buffer-name (a:make-gensym :spritesheet))
+    (setf %buffer-id (gpu:create-buffer %buffer-name :spritesheet))
     ;; TODO: Have materials automatically calculate a binding point instead of
     ;; hard-coding.
-    (gpu:bind-buffer (gpu:buffer-name %buffer) 1)
+    (gpu:bind-buffer %buffer-name 1)
     (write-spritesheet-buffer spritesheet)))
 
 (defun update-sprite-index (sprite step)
