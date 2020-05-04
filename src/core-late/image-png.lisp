9af486f55c715d2c542a43cae1d2df8d5be4e2a0
Michael Fiano <mail@michaelfiano.com>
2020-05-04 19:15:47 -0400

    use pngload dev branch, instead of pngload-fast

--- src/core-late/image-png.lisp
+++ src/core-late/image-png.lisp
@@ -1,37 +1,37 @@
 (in-package #:virality)
 
 (defun get-png-channel-count (image)
-  (ecase (pngload-fast:color-type image)
+  (ecase (pngload:color-type image)
     (:greyscale 1)
     (:truecolour 3)
     (:truecolour-alpha 4)))
 
 (defun get-png-pixel-format (image)
-  (ecase (pngload-fast:color-type image)
+  (ecase (pngload:color-type image)
     (:greyscale :red)
     (:greyscale-alpha :rg)
     (:truecolour :rgb)
     (:truecolour-alpha :rgba)))
 
 (defun get-png-pixel-type (image)
-  (ecase (pngload-fast:bit-depth image)
+  (ecase (pngload:bit-depth image)
     (8 :unsigned-byte)
     (16 :unsigned-short)))
 
 (defun get-png-internal-format (image)
   (let ((channel-count (get-png-channel-count image))
-        (bit-depth (pngload-fast:bit-depth image)))
+        (bit-depth (pngload:bit-depth image)))
     (a:format-symbol :keyword "~a~d"
                      (subseq "RGBA" 0 channel-count)
                      bit-depth)))
 
 (defmethod %load-image ((type (eql :png)) path)
-  (let ((image (pngload-fast:load-file path :flatten t)))
+  (let ((image (pngload:load-file path :flatten t)))
     (make-instance 'image
                    :path path
-                   :width (pngload-fast:width image)
-                   :height (pngload-fast:height image)
+                   :width (pngload:width image)
+                   :height (pngload:height image)
                    :pixel-format (get-png-pixel-format image)
                    :pixel-type (get-png-pixel-type image)
                    :internal-format (get-png-internal-format image)
-                   :data (pngload-fast:data image))))
+                   :data (pngload:data image))))
--- virality.asd
+++ virality.asd
@@ -24,7 +24,7 @@
                #:jsown
                #:lparallel
                #:origin
-               #:pngload-fast
+               #:pngload
                #:queues.simple-queue
                #:sdl2
                #:shadow
