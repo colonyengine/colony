276a9c6f2ecf48d9012e08432d90dd6bc4ac19a3
Michael Fiano <mail@michaelfiano.com>
2020-05-04 19:15:06 -0400

    fix spritesheet to work with more correct umbra sprite shader

--- src/components/sprite.lisp
+++ src/components/sprite.lisp
@@ -53,10 +53,10 @@
           :with size = (make-array count)
           :for sprite :in %spec
           :for i :from 0
-          :do (destructuring-bind (&key id x y w h) sprite
+          :do (destructuring-bind (&key id x y w h &allow-other-keys) sprite
                 (when (and id x y w h)
-                  (setf (aref pos i) (v2:vec x y)
-                        (aref size i) (v2:vec w h)
+                  (setf (aref pos i) (vector x y)
+                        (aref size i) (vector w h)
                         (u:href %sprites id) i)))
           :finally (shadow:write-buffer-path buffer-name :pos pos)
                    (shadow:write-buffer-path buffer-name :size size))))
