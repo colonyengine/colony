fdb9f291e5c6f5e120939847e6acdc619a0af8ef
Peter Keller <psilord@cs.wisc.edu>
2020-06-27 21:30:48 -0500

    Make the example window bigger to support playing PtP.
    I realize this will cause consternation. :/ When we finally get
    some sort of ability to have seperate named define-context objects
    and you can pick the one you want when running virality:start,
    it should fix this.

--- examples/src/common.lisp
+++ examples/src/common.lisp
@@ -1,8 +1,8 @@
 (in-package #:virality-examples)
 
 (v:define-config :virality-examples ()
-  :window-width 1280
-  :window-height 720
+  :window-width 1920 ;; 1280
+  :window-height 1080 ;; 720
   :delta (float 1/120 1f0)
   :initial-scene '(("geometric-volumes" examples)))
 
