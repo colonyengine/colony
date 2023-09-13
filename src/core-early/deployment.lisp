a31c8937f35f8e73c39145c2a572a4e11b8caaef
Peter Keller <psilord@cs.wisc.edu>
2023-09-12 19:46:51 -0500

    Change DEPLOY-BINARY to use the right interface for VIRALITY:START.

--- src/core-early/deployment.lisp
+++ src/core-early/deployment.lisp
@@ -7,13 +7,13 @@
 
 (defvar *deployed-p* nil)
 
-(defun deploy-binary (file-name project scene &key compress-p)
+(defun deploy-binary (file-name config scene &key compress-p)
   #+sbcl
   (progn
     (setf *deployed-p* t)
     (sb-ext:save-lisp-and-die
      file-name
-     :toplevel (lambda () (start :project project :scene scene))
+     :toplevel (lambda () (start :config config :scene scene))
      :executable t
      :compression (when compress-p 9)))
   #-sbcl
