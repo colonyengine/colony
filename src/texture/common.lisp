f020aea77d7574bdf40a51126410ec7e87647184
Peter Keller <psilord@cs.wisc.edu>
2025-05-10 00:03:29 -0500

    The function in this file got moved to vutils.

--- src/texture/common.lisp
+++ src/texture/common.lisp
@@ -58,27 +58,6 @@ constant and will never be changed at runtime.")
   "This variable converts between an easy human (semantic) name for a cube map
  face to the complicated opengl name.")
 
-(defun compute-mipmap-levels (width height &optional (depth 1))
-  "Compute how many mipmaps and what their resolutions must be given a WIDTH,
-HEIGHT, and DEPTH (which defaults to 1) size of a texture. We follow Opengl's
-formula in dealing with odd sizes (being rounded down). Return a values of: the
-number of mipmap levels the list of resolutions from biggest to smallest each
-mip map must have."
-  (flet ((round-down (x)
-           (ceiling (- x 1/2))))
-    (let ((num-levels (1+ (floor (log (max width height depth) 2))))
-          resolutions)
-      (push (list width height depth) resolutions)
-      (loop :with new-width = width
-            :with new-height = height
-            :with new-depth = depth
-            :for level :below (1- num-levels)
-            :do (setf new-width (max (round-down (/ new-width 2)) 1)
-                      new-height (max (round-down (/ new-height 2)) 1)
-                      new-depth (max (round-down (/ new-depth 2)) 1))
-                (push (list new-width new-height new-depth) resolutions))
-      (values num-levels (nreverse resolutions)))))
-
 (defun reshape-image-array-layout (images-array)
   "Reshape an array in the form of:
  #(#(a0 a1 a2 ...) #(b0 b1 b2 ...) ...)
